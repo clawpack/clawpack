1243a413b5229a448e9de1fb716fefbe9408d494
Aron Ahmadia <aron@ahmadia.net>
2012-04-04 17:21:31 +0300

    added git submodule hack to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+class build_with_submodules(build):
+    def run(self):
+        if path.exists('.git'):
+            check_call(['git', 'submodule', 'init'])
+            check_call(['git', 'submodule', 'update'])
+        build.run(self)
+
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
 
@@ -15,4 +22,5 @@ if __name__ == '__main__':
         from numpy.distutils.core import setup
     except Exception, err:
         sys.stderr.write('Unable to import numpy, please install numpy first before installing clawpack!')
-    setup(**configuration(top_path='').todict())
+    setup(cmdclass={"build": build_with_submodules},
+          **configuration(top_path='').todict())
