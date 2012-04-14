d7f7609d7ed79cb58a6997734532797ce92fcd31
Aron Ahmadia <aron@ahmadia.net>
2012-04-15 02:05:44 +0300

    massive restructuring to clawpack.

---
+++ clawpack/setup.py
@@ -0,0 +1,14 @@
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('clawpack',parent_package,top_path)
+    config.add_subpackage('clawutil')
+    config.add_subpackage('riemann')
+    config.add_subpackage('visclaw')
+    config.add_subpackage('pyclaw')
+    config.add_subpackage('petclaw')
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
