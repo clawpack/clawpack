b6851f53871daf69d447853373616b94949f7a2b
Aron Ahmadia <aron@ahmadia.net>
2012-04-14 23:54:22 +0300

    Revert "restructured location of sub-modules"
    This reverts commit 4d5ea4853fb6a094bd7059fa966e2100c545abf6.

--- setup.py
+++ setup.py
@@ -1,198 +1,32 @@
 #!/usr/bin/env python
-"""Clawpack: Python-based Clawpack installer
-"""
 
-# much of the functionality of this file was taken from the scipy setup.py script.
+try:
+    from numpy.distutils.core import setup
+except Exception, err:
+    sys.stderr.write('Unable to import numpy, please install numpy first before installing clawpack!')
 
-DOCLINES = __doc__.split("\n")
+from numpy.distutils.command.build import build
+from numpy.distutils.exec_command import exec_command
 
-import os
-import sys
-import warnings
-import subprocess
-import shutil
-import re
+import os.path
 
-if sys.version_info[0] < 3:
-    import __builtin__ as builtins
-else:
-    import builtins
-
-CLASSIFIERS = """\
-Development Status :: 4 - Beta
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-License :: OSI Approved
-Programming Language :: C
-Programming Language :: Python
-Topic :: Software Development
-Topic :: Scientific/Engineering
-Operating System :: POSIX
-Operating System :: Unix
-Operating System :: MacOS
-
-"""
-
-MAJOR               = 0
-MINOR               = 1
-MICRO               = 0
-ISRELEASED          = False
-VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-
-package_path       = os.path.join(os.path.dirname(__file__),'src')
-
-if not os.path.exists(package_path):
-    os.mkdir(package_path)
-if not os.path.exists(os.path.join(package_path),'__init__.py')
-    os.
-
-version_file_path  = os.path.join(package_path,'version.py')
-
-# Return the git revision as a string
-def git_version():
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = "Unknown"
-
-    return GIT_REVISION
-
-
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
-
-def write_version_py(filename=version_file_path):
-    cnt = """
-# THIS FILE IS GENERATED FROM CLAWPACK SETUP.PY
-short_version = '%(version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-git_revision = '%(git_revision)s'
-release = %(isrelease)s
-
-if not release:
-    version = full_version
-"""
-    # Adding the git rev number needs to be done inside
-    # write_version_py(), otherwise the import of scipy.version messes
-    # up the build under Python 3.
-    FULLVERSION = VERSION
-    if os.path.exists('.git'):
-        GIT_REVISION = git_version()
-    elif os.path.exists(version_file_path):
-        # must be a source distribution, use existing version file
-        from pyclaw.version import git_revision as GIT_REVISION
-    else:
-        GIT_REVISION = "Unknown"
-
-    if not ISRELEASED:
-        FULLVERSION += '.dev-' + GIT_REVISION[:7]
-
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % {'version': VERSION,
-                       'full_version' : FULLVERSION,
-                       'git_revision' : GIT_REVISION,
-                       'isrelease': str(ISRELEASED)})
-    finally:
-        a.close()    
+class build_with_submodules(build):
+    def run(self):
+        if os.path.exists('.git'):
+            exec_command(['git', 'submodule', 'init'])
+            exec_command(['git', 'submodule', 'update'])
+        build.run(self)
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
 
-    config = Configuration('clawpack', parent_package, top_path)
-    #    config.add_subpackage('clawutil')
-    #    config.add_subpackage('riemann')
-    #    config.add_subpackage('visclaw')
-    #    config.add_subpackage('pyclaw')
-
-    config.get_version('src/version.py')
+    config = Configuration(None, parent_package, top_path)
+    config.add_subpackage('clawutil')
+    config.add_subpackage('riemann')
+    config.add_subpackage('visclaw')
+    config.add_subpackage('pyclaw')
     return config
 
-
-def setup_package():
-    import sys
-    # Rewrite the version file everytime
-
-    old_path = os.getcwd()
-    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    src_path = local_path
-
-    os.chdir(local_path)
-    sys.path.insert(0, local_path)
-
-    old_path = os.getcwd()
-    os.chdir(src_path)
-    sys.path.insert(0, src_path)
-
-    write_version_py()
-
-    setup_dict = dict(
-        name = 'clawpack',
-        maintainer = "Clawpack Developers",
-        maintainer_email = "claw-dev@googlegroups.com",
-        description = DOCLINES[0],
-        long_description = "\n".join(DOCLINES[2:]),
-        url = "http://www.clawpack.org",
-        download_url = "https://github.com/clawpack/", 
-        license = 'BSD',
-        classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-        platforms = ["Linux", "Solaris", "Mac OS-X", "Unix"],
-        package_dir={'clawpack':'src'},
-        configuration=configuration,
-        )
-
-    try:
-        if 'egg_info' in sys.argv:
-            # only egg information for downloading requirements
-            from setuptools import setup
-            setuptools_dict = dict(
-                install_requires = ['numpy >= 1.6',
-                                    'matplotlib >= 1.0.1',
-                                    ],                            
-                extras_require = {'petclaw': ['petsc4py >= 1.2'],
-                                  'euler'  : ['scipy >= 0.10.0']},
-                )
-            setup_dict.update(setuptools_dict)
-            setup(**setup_dict)
-        else:
-            # actual build, numpy.distutils-based
-            from numpy.distutils.command.build import build
-            class build_with_submodules(build):
-                def run(self):
-                    if os.path.exists('.git'):
-                        from numpy.distutils.exec_command import exec_command
-                        exec_command(['git', 'submodule', 'init'])
-                        exec_command(['git', 'submodule', 'update'])
-                        build.run(self)
-
-            from numpy.distutils.core import setup
-            setup_dict.update(configuration().todict())
-            setup(cmdclass={"build": build_with_submodules},
-            **setup_dict)
-    except Exception as err:
-        print err
-    finally:
-        del sys.path[0]
-        os.chdir(old_path)
-    return
-
 if __name__ == '__main__':
-    setup_package()
+    setup(cmdclass={"build": build_with_submodules},
+          **configuration(top_path='').todict())
