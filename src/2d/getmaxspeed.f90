b82d4f58d77e83ec9da23405dd35b9556c0aef9d
Kyle Mandli <kyle.mandli@gmail.com>
2012-09-26 18:44:44 -0500

    Fix density division bugs

--- src/2d/getmaxspeed.f90
+++ src/2d/getmaxspeed.f90
@@ -26,8 +26,8 @@ real(kind=8) pure function get_max_speed(val,mitot,mjtot,nvar,aux,naux,nghost,hx
                 hxphys = xmetric * hx
                 h  = val(1,i,j) / rho(1)
                 if (h .gt. dry_tolerance(1)) then
-                    u  = val(2,i,j)/h
-                    v  = val(3,i,j)/h
+                    u  = val(2,i,j)/val(1,i,j)
+                    v  = val(3,i,j)/val(1,i,j)
                 else
                     u = 0.d0
                     v = 0.d0
@@ -41,8 +41,8 @@ real(kind=8) pure function get_max_speed(val,mitot,mjtot,nvar,aux,naux,nghost,hx
             do i = nghost+1, mitot-nghost
                 h  = val(1,i,j) / rho(1)
                 if (h .gt. dry_tolerance(1)) then
-                    u  = val(2,i,j)/h
-                    v  = val(3,i,j)/h
+                    u  = val(2,i,j)/val(1,i,j)
+                    v  = val(3,i,j)/val(1,i,j)
                 else
                     u = 0.d0
                     v = 0.d0
