ce3bb071d4553bcbb8dd6155a2afa55820e80f09
Kyle Mandli <kyle.mandli@gmail.com>
2012-10-09 11:57:05 -0500

    Variable name changes

--- src/2d/filval_new.f90
+++ src/2d/filval_new.f90
@@ -1,7 +1,7 @@
 !
 ! :::::::::::::::::::::::::::::: FILVAL ::::::::::::::::::::::::::
 !
-! create and fill coarser (lev-1) patch with one extra coarse cell all
+! create and fill coarser (level-1) patch with one extra coarse cell all
 ! around, plus the ghost cells . will interpolate from this patch to grid mptr
 ! without needing special boundary code.
 !
@@ -10,7 +10,7 @@
 !
 !
 ! ------------------------------------------------------------------
-subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
+subroutine filval(val, mx, my, dx, dy, level, time, valc, auxc, mic, &
                   mjc, xleft, xright, ybot, ytop, nvar, mptr, ilo, ihi, &
                   jlo, jhi, aux, naux, locflip, sp_over_h)
 
@@ -22,19 +22,19 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
     implicit none
 
     ! Input
-    integer, intent(in) :: mitot, mjtot, lev, mic, mjc, nvar, mptr, ilo, ihi
+    integer, intent(in) :: mx, my, level, mic, mjc, nvar, mptr, ilo, ihi
     integer, intent(in) :: jlo, jhi, naux, locflip
-    real(kind=8), intent(in) :: hx, hy, time, xleft, xright, ybot, ytop
+    real(kind=8), intent(in) :: dx, dy, time, xleft, xright, ybot, ytop
     real(kind=8), intent(in) :: valc(nvar,mic,mjc), auxc(naux,mic,mjc)
 
     ! Output
     real(kind=8), intent(in out) :: sp_over_h
-    real(kind=8), intent(in out) :: val(nvar,mitot,mjtot), aux(naux,mitot,mjtot)
+    real(kind=8), intent(in out) :: val(nvar,mx,my), aux(naux,mx,my)
 
     ! Local storage
-    integer :: levc, lratiox, lratioy, iclo, jclo, ichi, jchi, ng, i, ico, ifine
+    integer :: refinement_ratio_x, refinement_ratio_y, iclo, jclo, ichi, jchi, ng, i, ico, ifine
     integer :: ii, ivar, j, jco, jfine, jj
-    real(kind=8) :: coarseval(3), hxcrse, hycrse, xl, xr, yb, yt, area
+    real(kind=8) :: coarseval(3), dx_coarse, dy_coarse, xl, xr, yb, yt, area
     real(kind=8) :: dividemass, finemass, hvf, s1m, s1p, slopex, slopey, vel
     real(kind=8) :: velmax, velmin, vf, vnew, xoff, yoff
     logical :: fineflag(3)
@@ -42,49 +42,44 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
     ! External function definitions
     real(kind=8) :: get_max_speed
 
-
-    ! indext into eta array for surface values:
-    ! iaddeta(i,j) = loceta + i-1 + mic*(j-1)
-
-    levc    = lev - 1
-    lratiox = intratx(levc)
-    lratioy = intraty(levc)
-    hxcrse  = hx * lratiox
-    hycrse  = hy * lratioy
-    xl      = xleft  - hxcrse
-    xr      = xright + hxcrse
-    yb      = ybot   - hycrse
-    yt      = ytop   + hycrse
+    refinement_ratio_x = intratx(level - 1)
+    refinement_ratio_y = intraty(level - 1)
+    dx_coarse  = dx * refinement_ratio_x
+    dy_coarse  = dy * refinement_ratio_y
+    xl      = xleft  - dx_coarse
+    xr      = xright + dx_coarse
+    yb      = ybot   - dy_coarse
+    yt      = ytop   + dy_coarse
 
     ! set integer indices for coarser patch enlarged by 1 cell
     ! (can stick out of domain). proper nesting will insure this one
     ! call is sufficient.
-    iclo   = ilo / lratiox - 1
-    jclo   = jlo / lratioy - 1
-    ichi   = (ihi + 1) / lratiox - 1 + 1
-    jchi   = (jhi + 1) / lratioy - 1 + 1
+    iclo   = ilo / refinement_ratio_x - 1
+    jclo   = jlo / refinement_ratio_y - 1
+    ichi   = (ihi + 1) / refinement_ratio_x - 1 + 1
+    jchi   = (jhi + 1) / refinement_ratio_y - 1 + 1
     ng     = 0
 
     if (naux == 0) then
         if (xperdom .or. yperdom .or. spheredom) then
-            call preintcopy(valc,mic,mjc,nvar,iclo,ichi,jclo,jchi,levc,locflip)
+            call preintcopy(valc,mic,mjc,nvar,iclo,ichi,jclo,jchi,level - 1,locflip)
         else
-            call intcopy(valc,mic,mjc,nvar,iclo,ichi,jclo,jchi,levc,1,1)
+            call intcopy(valc,mic,mjc,nvar,iclo,ichi,jclo,jchi,level - 1,1,1)
         endif
     else  
         ! intersect grids and copy all (soln and aux)
         if (xperdom .or. yperdom .or. spheredom) then
             call preicall(valc,auxc,mic,mjc,nvar,naux,iclo,ichi,jclo,jchi, &
-                          levc,locflip)
+                          level - 1,locflip)
         else
-            call icall(valc,auxc,mic,mjc,nvar,naux,iclo,ichi,jclo,jchi,levc,1,1)
+            call icall(valc,auxc,mic,mjc,nvar,naux,iclo,ichi,jclo,jchi,level - 1,1,1)
         endif
     endif
-    call bc2amr(valc,auxc,mic,mjc,nvar,naux,hxcrse,hycrse,levc,time,xl,xr,yb, &
+    call bc2amr(valc,auxc,mic,mjc,nvar,naux,dx_coarse,dy_coarse,level - 1,time,xl,xr,yb, &
                 yt,xlower,ylower,xupper,yupper,xperdom,yperdom,spheredom)
 
     !-----------------------------
-    ! For shallow water over topography, in coarse cells convert from h to eta,
+    ! For shallow water over topograpdy, in coarse cells convert from h to eta,
     ! before interpolating:
     !-----------------------------
 
@@ -119,17 +114,17 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
 
             ! Interpolate from coarse cells to fine grid to find depth
             finemass = 0.d0
-            do ico = 1,lratiox
-                do jco = 1,lratioy
-                    yoff = (real(jco,kind=8) - 0.5d0) / lratioy - 0.5d0
-                    xoff = (real(ico,kind=8) - 0.5d0) / lratiox - 0.5d0
-                    jfine = (j-2) * lratioy + nghost + jco
-                    ifine = (i-2) * lratiox + nghost + ico
+            do ico = 1,refinement_ratio_x
+                do jco = 1,refinement_ratio_y
+                    yoff = (real(jco,kind=8) - 0.5d0) / refinement_ratio_y - 0.5d0
+                    xoff = (real(ico,kind=8) - 0.5d0) / refinement_ratio_x - 0.5d0
+                    jfine = (j-2) * refinement_ratio_y + nghost + jco
+                    ifine = (i-2) * refinement_ratio_x + nghost + ico
                     val(1,ifine,jfine) = (coarseval(2) + xoff * slopex &
                                                        + yoff * slopey) * rho(1)
                     val(1,ifine,jfine) = max(0.d0, val(1,ifine,jfine) / rho(1) &
                                             - aux(1,ifine,jfine)) * rho(1)
-                    finemass = finemass + val(1,ifine,jfine)
+                    finemass = finemass + val(1,ifine,jfine) / rho(1)
                     if (val(1,ifine,jfine) / rho(1) <= dry_tolerance(1)) then
                         fineflag(1) = .true.
                         val(2,ifine,jfine) = 0.d0
@@ -142,7 +137,7 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
             ! finemass is the total mass in all new fine grid cells
             ! all fine mass has been determined for this coarse grid cell
             ! if all fine cells are dry, momentum has already been set
-            if (finemass / rho(1) >= dry_tolerance(1)) then
+            if (finemass >= dry_tolerance(1)) then
                 do ivar = 2,nvar
                     fineflag(ivar)=.false.
                     s1p = (valc(ivar,i+1,j) - valc(ivar,i,j)) / rho(1)
@@ -179,14 +174,14 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
                     enddo
 
                     ! try to set momentum
-                    do ico = 1,lratiox
+                    do ico = 1,refinement_ratio_x
                         if (fineflag(1).or.fineflag(ivar)) exit
 
-                        do jco = 1,lratioy
-                            jfine = (j-2) * lratioy + nghost + jco
-                            ifine = (i-2) * lratiox + nghost + ico
-                            yoff = (real(jco,kind=8) - 0.5d0) / lratioy - 0.5d0
-                            xoff = (real(ico,kind=8) - 0.5d0) / lratiox - 0.5d0
+                        do jco = 1,refinement_ratio_y
+                            jfine = (j-2) * refinement_ratio_y + nghost + jco
+                            ifine = (i-2) * refinement_ratio_x + nghost + ico
+                            yoff = (real(jco,kind=8) - 0.5d0) / refinement_ratio_y - 0.5d0
+                            xoff = (real(ico,kind=8) - 0.5d0) / refinement_ratio_x - 0.5d0
                             hvf = valc(ivar,i,j) / rho(1) + xoff * slopex &
                                                           + yoff*slopey
                             vf = hvf / (val(1,ifine,jfine) / rho(1))
@@ -203,14 +198,14 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
                     ! generating new extrema in velocities
                     if (fineflag(1) .or. fineflag(ivar)) then
                         ! more mass now, conserve momentum
-                        area = real(lratiox * lratioy,kind=8)
-                        dividemass = max(finemass,valc(1,i,j))
-                        Vnew = area * valc(ivar,i,j) / dividemass
+                        area = real(refinement_ratio_x * refinement_ratio_y,kind=8)
+                        dividemass = max(finemass,valc(1,i,j) / rho(1))
+                        Vnew = area * valc(ivar,i,j) / (dividemass * rho(1))
 
-                        do ico = 1,lratiox
-                            do jco = 1,lratioy
-                                jfine = (j-2) * lratioy + nghost + jco
-                                ifine = (i-2) * lratiox + nghost + ico
+                        do ico = 1,refinement_ratio_x
+                            do jco = 1,refinement_ratio_y
+                                jfine = (j-2) * refinement_ratio_y + nghost + jco
+                                ifine = (i-2) * refinement_ratio_x + nghost + ico
                                 val(ivar,ifine,jfine) = Vnew * val(1,ifine,jfine)
                             enddo
                         enddo
@@ -223,8 +218,8 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
     enddo !end of coarse loop
 
     ! overwrite interpolated values with fine grid values, if available.
-    call intcopy(val,mitot,mjtot,nvar,ilo-nghost,ihi+nghost,jlo-nghost, &
-                 jhi+nghost,lev,1,1)
+    call intcopy(val,mx,my,nvar,ilo-nghost,ihi+nghost,jlo-nghost, &
+                 jhi+nghost,level,1,1)
 
     ! scan for max wave speed on newly created grid. this will be used to set appropriate
     ! time step and appropriate refinement in time. For this app not nec to refine by same
@@ -232,7 +227,7 @@ subroutine filval(val, mitot, mjtot, hx, hy, lev, time, valc, auxc, mic, &
     ! speeds.
 
     if (varRefTime) then   ! keep consistent with setgrd_geo and qinit_geo
-        sp_over_h = get_max_speed(val,mitot,mjtot,nvar,aux,naux,nghost,hx,hy)
+        sp_over_h = get_max_speed(val,mx,my,nvar,aux,naux,nghost,dx,dy)
     endif
 
 end subroutine filval
