dc422c7e93861c195dd79e67893baa653992d67b
David Ketcheson <dketch@gmail.com>
2012-10-16 11:06:42 +0300

    Don't import subpackages by default.

--- clawpack/__init__.py
+++ clawpack/__init__.py
@@ -1,13 +1,1 @@
-
-try:
-    import clawutil
-    import pyclaw
-    import petclaw
-    import riemann
-    import visclaw
-
-    __all__ = ['clawutil','pyclaw','petclaw','riemann','visclaw']
-except ImportError:
-    pass
-
-# need top-level try/except protection because this package may be accessed mid-install
+__all__ = ['clawutil','pyclaw','petclaw','riemann','visclaw']
