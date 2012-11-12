4404ce486afc5f183e04096aa4ae50056b17d762
Kyle Mandli <kyle.mandli@gmail.com>
2012-11-12 16:28:43 -0600

    Update for new data structures

--- src/2d/storm_surge/setprob.f90
+++ src/2d/storm_surge/setprob.f90
@@ -1,5 +1,7 @@
 subroutine setprob()
 
+    use regions_module, only: set_regions
+    use gauges_module, only: set_gauges
     use geoclaw_module, only: set_geo
     use topo_module, only: read_topo_settings
     use dtopo_module, only: read_dtopo_settings
@@ -10,13 +12,12 @@ subroutine setprob()
     
     implicit none
 
-    call set_geo()                    !# sets basic parameters g and coord system
-    call set_refinement()             !# sets refinement control parameters
-    call read_topo_settings()         !# specifies topography (bathymetry) files
-    call read_dtopo_settings()        !# specifies file with dtopo from earthquake
-    call set_qinit()                  !# specifies file with dh if this used instead
-    call set_fixed_grids()            !# Fixed grid settings
-    call setgauges()                  !# locations of measuring gauges
+    call set_geo()                    ! sets basic parameters g and coord system
+    call set_refinement()             ! sets refinement control parameters
+    call read_topo_settings()         ! specifies topography (bathymetry) files
+    call read_dtopo_settings()        ! specifies file with dtopo from earthquake
+    call set_qinit()                  ! specifies file with dh if this used instead
+    call set_fixed_grids()            ! Fixed grid settings
 
     call set_storm()                  ! Set storm parameters
     
