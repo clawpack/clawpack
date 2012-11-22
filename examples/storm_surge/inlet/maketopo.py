d9f13a55c18570c3d94b115d1f1641ad0817b612
Kyle Mandli <kyle.mandli@gmail.com>
2012-11-21 23:21:20 -0800

    Inlet test basic working, no forcing

--- examples/storm_surge/inlet/maketopo.py
+++ examples/storm_surge/inlet/maketopo.py
@@ -7,215 +7,250 @@ Functions for creating topography
 
 import os
 import sys
+
 import numpy as np
+from scipy.interpolate import griddata
 
-from clawpack.clawutil.data import Data
-import geoclaw.topotools as tt
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors 
 
-# ============================================================================
-#  Profiles
-def generate_profiles():
-    # Parameters
-    data = Data('./amr2ez.data')
-    dx = abs(data.xupper-data.xlower) / (data.mx)
-    dy = abs(data.yupper-data.ylower) / (data.my)
-    d = min(dx,dy)
-    mx = int((data.xupper-data.xlower) / d) + 8
-    my = int((data.yupper-data.ylower) / d) + 8
+import clawpack.visclaw.colormaps as colormaps
+import bathy
 
-    xlower = data.xlower-d*4.0
-    ylower = data.ylower-d*4.0
-    xupper = data.xupper+d*4.0
-    yupper = data.yupper+d*4.0
+# Degree to meter conversion function
+R_earth = 6378.1 * 1000.0
+deg2meters = lambda theta,lat:R_earth * theta * np.pi / 180.0 * np.cos(lat * np.pi / 180.0)
+meters2deg = lambda d,lat:d / (R_earth * np.pi / 180.0 * np.cos(lat * np.pi / 180.0))
 
-    beach_slope = 0.05
-    basin_depth = -3000
-    shelf_depth = -200
-    x0 = 350e3
-    x1 = 450e3
-    x2 = 480e3
+# Read in fort.14 file
+def extract_bathy(path, extent, no_data_value=999999, method='nearest', 
+                                delta_limit=20.0, TOLERANCE=1e-3):
+    r"""Extract sub-section of bathymetry from file at path
 
-    shelf_slope = (basin_depth - shelf_depth) / (x0 - x1)
-    y_end = beach_slope * (xupper - 477e3) - 100.0
-    
-    # Stommel problem depth
-    stommel_depth = -1000
-    
-    # Points and depths for gulf shelf
-    #  1: (25°39'2.85"N, 86° 7'24.77"W)   --   -3228 m   --   0.0 m
-    #  2: (27°53'44.74"N, 88° 0'34.02"W)   --   -2438 m   --   312.17313 km
-    #  3: (28°59'47.14"N, 88°59'53.19"W)   --   -188 m   --    467.59957 km
-    #  4: ( 29° 4'6.90"N,  89° 4'11.39"W)    --   0 m   --   479.10557 km
+    Function to extract a sub-section given by extent of the bathymetry file at
+    path assumed to be in a x,y,z format which can be unstructured.  Returns the
+    data interpolated onto a grid determined by the resolution of the original
+    file or the limiting resolution delta_limit.
 
-    bathy_profiles = {"simple_shelf":[(xlower,basin_depth),
-                                      (x0,basin_depth),
-                                      (x1,shelf_depth),
-                                      (x2,shelf_depth),
-                                      (xupper,beach_slope*(xupper-x2)+shelf_depth)],
-                      "shallow_shelf":[(xlower,-100),
-                                       (x2,-100),
-                                       (xupper,y_end)],
-                      "gulf_shelf":[(0.0,-3228),
-                                    (312e3,-2438),
-                                    (467e3,-188),
-                                    (479e3,0.0),
-                                    (579e3,300.0)],
-                      "step_shelf1":[(0.0,-2000.0),
-                                     (470e3-0.001,-2000.0),
-                                     (470e3,-200.0),
-                                     (500e3,-200.0)],
-                      "shelf":[(0.0,-3000),
-                               (400e3,-2700),
-                               (450e3,-100),
-                               (500e3,-100)],
-                      "continental_shelf":[(2000e3,-7000),
-                                           (2800e3,-3000),
-                                           (2900e3,-100),
-                                           (3000e3,0.0)],
-                      "flat100":[(0.0,-100),
-                              (1000e3,-100)],
-                      "flat1000":[(0.0,-1000),
-                              (1000e3,-1000)],
-                      "flat_stommel":[(0.0,stommel_depth),
-                                      (1000e3,stommel_depth)]}
-    return bathy_profiles
+    :Input:
+     *path* (string) - Path to the bathymetry file which the data is being 
+                       pulled from.
+     *extent* (tuple) - A tuple defining the rectangle of the sub-section.  Must
+                        be in the form (x lower,x upper,y lower, y upper).
+     *no_data_value* (float) - Value to use if no data was found to fill in a 
+                               missing value, ignored if `method = 'nearest'`.
+                               Default is `999999`.
+     *method* (string) - Method for interpolation, valid methods are found in
+                         the scipy module scipy.interpolate.  Default is 
+                         `nearest`.
+     *delta_limit* (float) - Limit of finest horizontal resolution, default is
+                             20 meters.
+     *tolerance* (float) -  Tolerance allowed for extent matching.  Since the 
+                            requested extents and the eventual output may not 
+                            match due to round off, this parameter is used to 
+                            check if they are within acceptable tolerances.
+                            Default is `1e-3`.
 
-# ============================================================================
-#  Topography generation functions 
-# ============================================================================
-def write_topo_file(topo_file,topo_type=1,factor=4,bathy_type=None,plot=False,
-                        force=False,verbose=False):
-    """Creates topography file needed by the simulation"""
-    
-    if os.path.exists(topo_file):
-        if not force:
-            ans = raw_input("The file %s already exists, replace: [Y/n] " % topo_file)
-            if ans[0].capitalize() == 'N':
-                print "File %s already exists, use force if you want to replace the existing file." % topo_file
-                return
-    print "Creating topography file ",topo_file
-    
-    # Look at parameters of domain
-    data = Data('./amr2ez.data')
-    dx = abs(data.xupper-data.xlower) / (data.mx)
-    dy = abs(data.yupper-data.ylower) / (data.my)
-    d = min(dx,dy)
-    mx = int((data.xupper-data.xlower) / d) + 8
-    my = int((data.yupper-data.ylower) / d) + 8
+    :Output:
+     *Z* (ndarray) - Interpolated 2D array of bathymetry depths starting in the
+                     upper right corner of the sub-section specified by extent.
+     *delta* (float) - Final choice used for the horizontal resolution.
+    """
 
-    xlower = data.xlower-d*4.0
-    ylower = data.ylower-d*4.0
-    xupper = data.xupper+d*4.0
-    yupper = data.yupper+d*4.0
-    
-    bathy_profiles = generate_profiles()
+    # Extract data
+    print "Loading data from file %s" % path
+    points = []
+    values = []
+    dx = np.infty
+    dy = np.infty
 
-    # Pick out bathymetry profile
-    if bathy_type is None:
-        bathy_profile = 'flat'
-    if bathy_type in bathy_profiles.keys():
-        bathy_profile = bathy_profiles[bathy_type]
+    data_file = open(path,'r')
+    data_file.readline()
+    num_lines = int(data_file.readline().split()[1])
+
+    data = np.empty((3,num_lines))
+
+    for n in xrange(num_lines):
+        line = data_file.readline().split()
+        coordinates = [float(value) for value in line[1:]]
+        if extent[0] <= coordinates[0] <= extent[1]:
+            if extent[2] <= coordinates[1] <= extent[3]:
+                points.append(coordinates[0:2])
+                values.append(coordinates[2])
+
+                # Try to determine smallest dx and dy
+                if len(points) > 1:
+                    if np.abs(points[-1][0] - points[-2][0]) < dx:
+                        dx = np.abs(points[-1][0] - points[-2][0])
+                    if np.abs(points[-1][1] - points[-2][1]) < dy:
+                        dy = np.abs(points[-1][1] - points[-2][1])
+
+    data_file.close()
+
+    if len(points) == 0:
+        raise Exception("No points were found inside requested extent.")
+
+    # Cast lists as ndarrays
+    points = np.array(points)
+    values = np.array(values)    
+
+    # Create regularized grid
+    print "Computing grid data"
+    delta = max(min(dx,dy),delta_limit) # Limit to size of delta
+    N = (np.ceil((extent[1] - extent[0]) / delta),
+         np.ceil((extent[3] - extent[2]) / delta))
+    print "  delta = %s, N = %s" % (delta,N)
+    if N[0] > 2000 or N[1] > 2000:
+        raise Exception("Calculated resolution %s too high!")
+    x = np.linspace(extent[0],extent[1],N[0])
+    y = np.linspace(extent[2],extent[3],N[1])
+    X,Y = np.meshgrid(x,y)
+
+    # Check extents
+    if abs(x[0]  - extent[0]) > TOLERANCE or \
+       abs(x[-1] - extent[1]) > TOLERANCE or \
+       abs(y[0]  - extent[2]) > TOLERANCE or \
+       abs(y[-1] - extent[3]) > TOLERANCE:
+
+       raise Exception("Calculated grid out of extent tolerance.")
+
+    # Interpolate known points onto regularized grid
+    print "Creating interpolating function..."
+    Z = griddata(points,values,(X,Y), method=method, fill_value=no_data_value)
+
+    return Z,delta
+
+
+def write_bathy(path,Z,lower,delta,no_data_value=999999,topotype=3):
+    r"""Write out a topography file to path of type topotype
+
+    Writes out a bathymetry file of type 3 to path from data in Z.  The rest of
+    the arguments are used to write the header data.
+    """
+
+    outfile = open(path,'w')
+
+    if topotype == 3:
+        # Write out header
+        outfile.write('%s ncols\n' % Z.shape[1])
+        outfile.write('%s nrows\n' % Z.shape[0])
+        outfile.write('%s xll\n' % lower[0])
+        outfile.write('%s yll\n' % lower[1])
+        outfile.write('%s cellsize\n' % delta)
+        outfile.write('%s nodata_value\n' % no_data_value)
+
+        # Write out bathy data
+        # We flip the output data here since we write from the upper left corner
+        # to lower right and the data is ordered from lower left to upper right
+        Z_flipped = np.flipud(Z)
+        for i in xrange(Z.shape[0]):
+            for j in xrange(Z.shape[1]):
+                outfile.write("%s   " % (Z_flipped[i,j]))
+            outfile.write("\n")
+
     else:
-        err_msg = "Bathy profile %s not found, available options are" % bathy_type
-        for name in bathy_profiles.keys():
-            err_msg = '\n   '.join((err_msg,name))
-        raise Exception(err_msg)
-    if verbose:
-        print "%s profile: %s" % (bathy_type,bathy_profile)
+        raise NotImplemented("Output type %s not implemented." % topotype)
 
-    # Create and write topography
-    bathy_func = tt.create_topo_func(bathy_profile,verbose=verbose)
-    N = len(bathy_profile)
-    if topo_type == 1:
-        tt.topo1writer(topo_file,bathy_func,xlower,xupper,ylower,yupper,
-            factor*mx,factor*my)
-    elif topo_type == 2:
-        print (xlower - xupper) / (factor*mx)
-        print (ylower - yupper) / (factor*my)
-        tt.topo2writer(topo_file,bathy_func,xlower,xupper,ylower,yupper,
-            factor*mx,factor*my)
-    else:
-        raise Exception("Unknown topo type %s requested" % topo_type)
+    outfile.close()
 
-    # Plotting
-    if plot:
-        plot_profiles({bathy_type:bathy_profile},factor=factor)
-        
-        
-def plot_profiles(profiles,topo_type=1,factor=4,verbose=True):
-    
-    import tempfile
-    import shutil
-    import matplotlib.pyplot as plt
-    
-    # Assume an amr2ez.data file is present
-    data = Data('./amr2ez.data')
-    dx = abs(data.xupper-data.xlower) / (data.mx)
-    dy = abs(data.yupper-data.ylower) / (data.my)
-    d = min(dx,dy)
-    mx = int((data.xupper-data.xlower) / d) + 8
-    my = int((data.yupper-data.ylower) / d) + 8
 
-    xlower = data.xlower-d*4.0
-    ylower = data.ylower-d*4.0
-    xupper = data.xupper+d*4.0
-    yupper = data.yupper+d*4.0
+def plot_bathy(paths,region_path,patch_edges=True,patch_names=True,names=None,
+               plot_coastline=True):
+    r"""Plot the bathymetry files specified in paths and region_path."""
     
-    bathy_path = tempfile.mkdtemp()
-    grid_size = int(np.ceil(np.sqrt(len(profiles.keys()))))
-    index = 1
-    plt.figure(1)
+    # Setup region figure
+    region_fig = plt.figure(1)
+    region_axes = region_fig.add_subplot(111)
+
+    # Setup patch figure
+    patch_fig = plt.figure(2)
+    columns = 3
+    rows = np.ceil(len(paths) / float(columns))
+    patch_axes = [patch_fig.add_subplot(rows,columns,i) for i in xrange(len(paths))]
+
+    # Read in region bathymetry
+    X,Y,Z = bathy.read_topo(region_path)
+    region_extent = (np.min(X),np.max(X),np.min(Y),np.max(Y))
+    depth_extent = (np.min(Z),np.max(Z))
+
+    # Create color map
+    cmap = colormaps.make_colormap({-1:[0.3,0.2,0.1],
+                                       -0.00001:[0.95,0.9,0.7],
+                                       0.00001:[.5,.7,0],
+                                       1:[.2,.5,.2]})
+    color_norm = colors.Normalize(depth_extent[0],depth_extent[1],clip=True)
+
+    # Plot region data
+    region_plot = region_axes.imshow(Z,vmin=depth_extent[0],
+                                       vmax=depth_extent[1],
+                                       extent=region_extent,
+                                       cmap=cmap,norm=color_norm)
     
-    for (name,profile) in profiles.iteritems():
-        topo_file = os.path.join(bathy_path,name)
-        
-        # Write out temporary file
-        bathy_func = tt.create_topo_func(profile,verbose=verbose)
-        N = len(profile)
-        factor = 4
-        if topo_type == 1:
-            tt.topo1writer(topo_file,bathy_func,xlower,xupper,ylower,yupper,
-                        factor*mx,factor*my)
-        elif topo_type == 2:
-            tt.topo2writer(topo_file,bathy_func,xlower,xupper,ylower,yupper,
-                        factor*mx,factor*my)
+    if plot_coastline:
+        region_axes.contour(X,Y,Z,levels=[0.0],colors='r')
+
+
+    # Read in and plot each patch
+    for (i,patch_path) in enumerate(paths):
+        X,Y,Z = bathy.read_topo(patch_path)
+        extent = (np.min(X),np.max(X),np.min(Y),np.max(Y))
+
+        # Plot on region figure
+        region_axes.imshow(Z,vmin=depth_extent[0],vmax=depth_extent[1],
+                             extent=extent,cmap=cmap,norm=color_norm)
+
+        # Plot boundaries of local bathy on region plot
+        if patch_edges:
+            # Bottom boundary
+            region_axes.plot((extent[0],extent[1]),(extent[2],extent[2]),'k')
+            # Upper boundary
+            region_axes.plot((extent[0],extent[1]),(extent[3],extent[3]),'k')
+            # Left boundary
+            region_axes.plot((extent[0],extent[0]),(extent[2],extent[3]),'k')
+            # Right boundary
+            region_axes.plot((extent[1],extent[1]),(extent[2],extent[3]),'k')
+
+        # Write name near edge
+        if names is None:
+            file_name = os.path.splitext(patch_path)[0]
         else:
-            raise Exception("Invalid topo_type %s." % topo_type)
-                        
-        # Read it back in
-        (X,Y,Z) = tt.topofile2griddata(topo_file,topotype=topo_type)
-        slice_index = int(X.shape[0] / 2.0)
-        x = X[slice_index,:]
-        b = Z[slice_index,:]
-        
-        limits = [np.min(x),np.max(x),np.min(b)*1.05,np.max(b)*1.05]
-        wet_index = np.nonzero(b < 0.0)
-        
-        plt.subplot(grid_size,grid_size,index)
-        plt.hold(True)
-        plt.plot(x,b,'k',linewidth=2)
-        plt.plot(x[wet_index],np.zeros(x[wet_index].shape),'b')
-        plt.axis(limits)
-        plt.title(name)
-        plt.xlabel('km')
-        plt.ylabel('m')
-        locs,labels = plt.xticks()
-        labels = locs/1e3
-        plt.xticks(locs,labels)
-        plt.hold(False)
-        index = index + 1
-        
-    plt.show()
-        
-    shutil.rmtree(bathy_path)
+            file_name = names[i]
+        if patch_names:
+            delta = X[0,0] - X[1,0]
+            region_axes.text(extent[0]+delta,extent[2]+delta,file_name,color='m')
+
+        # Plot on local bathy
+        patch_axes[i].imshow(Z,vmin=depth_extent[0],vmax=depth_extent[1],
+                             extent=extent,cmap=cmap,norm=color_norm)
+        patch_axes[i].set_title(file_name)
+        patch_axes[i].set_xlim(extent[0:2])
+        patch_axes[i].set_ylim(extent[2:])
+        patch_axes[i].set_xlabel('longitude')
+        patch_axes[i].set_ylabel('latitude')
+
+    # Fix up figures
+    region_axes.set_xlim(region_extent[0:2])
+    region_axes.set_ylim(region_extent[2:])
+    region_axes.set_title('Region')
+    region_fig.colorbar(region_plot)
+    
+    # patch_fig.colorbar(region_plot)
+
+    return region_fig,patch_fig
 
         
 if __name__ == "__main__":
-    # plot_profiles(generate_profiles())
-    # write_bathy_profile('./topo.data',None,plot=True,force=True)
-    # Create bathy
-    topo_file = './topo.tt1'
-    if len(sys.argv) > 1:
-        topo_file = sys.argv[1]
-    write_topo_file(topo_file,topo_type=1,factor=4,
-                            bathy_type='simple_shelf',plot=False,force=True)
+    bathy_file = './fort.14'
+    regions = {'full_bathy':[-2250.0, 2250.0, -1500.0, 1500.0]}
+    for region_name,extent in regions.iteritems():
+        out_path = "%s.tt3" % region_name
+        Z,delta = extract_bathy(bathy_file,extent)
+        print np.max(Z),np.min(Z)
+        write_bathy(out_path,-Z,(extent[0],extent[2]),delta)
+
+    # Plot results
+    plot_bathy(['.'.join((name,'tt3')) for name in regions.keys()],
+               'full_bathy.tt3',names=regions.keys(),patch_names=False)
+    plt.show()
+
+
