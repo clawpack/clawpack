b5550ff648d8ac21ae22d4b62ee19eb715ce6ede
Kyle Mandli <kyle.mandli@gmail.com>
2013-01-22 15:47:39 -0600

    Add GeoClaw simulation comparison to real gauge data

--- examples/storm_surge/gulf/ike/gauge_plot.py
+++ examples/storm_surge/gulf/ike/gauge_plot.py
@@ -2,16 +2,17 @@
 
 import os
 
-import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
+
+import clawpack.visclaw.gaugetools as gaugetools
 
 min2deg = lambda minutes: minutes / 60.0
 ft2m = lambda x:0.3048 * x
 
 
 # Parameters
-base_path = './'
+base_path = './Ike_gauge_data'
 Ike_landfall = 256
 
 # Load in station info
@@ -46,14 +47,19 @@ index = 0
 for (name,station_data) in stations.iteritems():
     index = index + 1
 
-    # Load and extract station data
+    # Load and extract real station data
     data = scipy.io.loadmat(os.path.join(base_path,'result_%s.mat' % name))
     days = data['yd_processed'][0,:] - Ike_landfall
     mean_water = data['mean_water'].transpose()[0,:]
 
+    # Load GeoClaw gauge data
+    gauge = gaugetools.GaugeSolution(index,location=station_data['location'])
+    gauge.read(output_path='./_output',file_name='fort.gauge')
+
     # Plot 
     axes = fig_Hs.add_subplot(2,len(stations)/2,index)
-    axes.plot(days,mean_water+station_data['depth'],'b')
+    axes.plot(days,mean_water+station_data['depth'],'k')
+    axes.plot(gauge.t - Ike_landfall,gauge.q[3,:],'.b')
     axes.set_xlabel('Landfall Day')
     axes.set_ylabel('Height (m)')
     axes.set_title("Station %s" % name)
