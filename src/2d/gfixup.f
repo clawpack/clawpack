5d0c36d3ed0c890f108135880059c9ff42ec2531
Kyle Mandli <kyle.mandli@gmail.com>
2013-01-28 13:48:50 -0600

    Possible solution to gfixup spoh=0 problem

--- src/2d/gfixup.f
+++ src/2d/gfixup.f
@@ -180,8 +180,16 @@ c  this is set during timestepping
 c
       do level = lbase+1, lfine   
          dtc = possk(level-1)
-!        dtf = cfl/(spoh(level) + tiny(1.d0)) ! orig code
+         if (abs(spoh(level)) < tiny(1.d0)) then
+           print *,"Floating point divide by zero possible."
+           print *,"Maybe due to completely dry grid."
+           print "(a,i2,1d16.8)","   spoh(",level,") = ",spoh(level)
+           print *,"   cfl = ",cfl
+           dtf = huge(1.d0)
+         else
+C        dtf = cfl/(spoh(level) + tiny(1.d0)) ! orig code
          dtf = cfl/(spoh(level) )  ! changed to match 4-x
+         endif
          if (dtf .gt. dtc) then
             kratio(level-1) = 1  ! cant have larger timestep than parent level
             possk(level)    = dtc  ! cant have larger timestep than parent level
