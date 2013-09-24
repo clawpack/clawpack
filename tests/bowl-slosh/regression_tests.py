d81e626a5c5c7e9e95aad8a9d302c58b00d733db
Randy LeVeque <rjl@ned>
2013-09-24 16:57:03 -0700

    improved regression tests based on changes to amrclaw tests

--- tests/bowl-slosh/regression_tests.py
+++ tests/bowl-slosh/regression_tests.py
@@ -1,6 +1,14 @@
 
+"""
+Regression tests.  Execute via:
+    python regression_tests.py
+to test, or
+    python regression_tests.py True
+to create new regression data for archiving.
+"""
+
 from clawpack.visclaw import data
-import os
+import os,sys
 import numpy as np
 import subprocess
 
@@ -27,33 +35,34 @@ def test2(save_new_regression_data=False):
     """
     Check Gauge 1, only test sum of t and sum of q values.
     """
+    
+    # unique for this test:
+    gaugeno = 1
+    fname_data = 'regression_data_test2.txt'
 
-    g1 = plotdata.getgauge(1)
-    tsum = g1.t.sum()
-    qsum = g1.q[0,:].sum()
+    g = plotdata.getgauge(gaugeno)
+    tsum = g.t.sum()
+    qsum = g.q[0,:].sum()
 
+    new_data = np.array([tsum,qsum])
+    
     if save_new_regression_data:
-        np.savetxt('regression_data_test2.txt',np.array([tsum,qsum]))
+        np.savetxt(fname_data, new_data)
+        print "*** Created new regression_data file ", fname_data
 
     # Read in archived data for comparison:
-    regression_data = np.loadtxt('regression_data_test2.txt')
-    tsum_expected = regression_data[0]
-    qsum_expected = regression_data[1]
+    regression_data = np.loadtxt(fname_data)
 
     tol = 1e-14
-    assert np.allclose(tsum,tsum_expected,tol), \
-        "gauge 1: tsum = %s, expected: %s"  % (tsum, tsum_expected)
-    assert np.allclose(qsum,qsum_expected,tol), \
-        "gauge 1: qsum = %s, expected: %s"  % (qsum, qsum_expected)
-    print "Gauge 1 OK"
-    
-    
+    assert np.allclose(new_data,regression_data,tol), \
+        "\n  new_data: %s, \n  expected: %s"  % (new_data, regression_data)
+    print "Gauge %i ok" % gaugeno
+
+
 if __name__=="__main__":
-    save_new_regression_data = False  # Set to True to archive new results
+    save_new_regression_data = (len(sys.argv) > 1) and (sys.argv[1]=='True')
     test1()
     test2(save_new_regression_data)
-    if save_new_regression_data:
-        print "*** Created new regression_data files ***"
     
 
 
