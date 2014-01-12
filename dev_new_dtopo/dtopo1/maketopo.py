ce27b6d482bbd902246ea92dec29df5e30c5fb70
Randy LeVeque <rjl@ned>
2014-01-11 17:16:49 -0800

    added dtopo1 test script and data needed for test problem

--- dev_new_dtopo/dtopo1/maketopo.py
+++ dev_new_dtopo/dtopo1/maketopo.py
@@ -4,7 +4,9 @@ Module to create topo and qinit data files for this example.
 """
 
 from clawpack.geoclaw.topotools import topo1writer, topo2writer
+from clawpack.geoclaw import dtopotools
 from numpy import *
+import os
 
 
 def maketopo():
@@ -43,8 +45,81 @@ def topo2(x,y):
     return z
 
 
+def read_subfaults_dtopo1(plotfig=None):
+    """
+    Test data
+    """
+    fname_subfaults = 'dtopo1.csv'
+
+    # Format of subfault file:
+    columns = """longitude latitude depth length width strike dip rake slip
+       """.split()
+    defaults = {'latlong_location': 'top center'}
+    units = {'slip': 'm', 'depth': 'km', 'length': 'km', 'width': 'km'}
+
+    subfaults = dtopotools.read_subfault_model(fname_subfaults, \
+                        columns=columns, units=units, \
+                        defaults = defaults, skiprows=1, delimiter=',')
+                    
+    if plotfig:
+        figure(plotfig)
+        dtopotools.plot_subfaults(subfaults,slip_color=True,plot_rake=True)
+        
+    return subfaults
+
+def make_dtopo_dtopo1(plotfig=None):
+    """
+    Test data.
+    """
+    from clawpack.geoclaw import okada2
+
+    subfaults = read_subfaults_dtopo1()
+
+    dtopo_fname = 'dtopo1.tt3'
+
+    if os.path.exists(dtopo_fname):
+        print "*** Not regenerating dtopo file (already exists): %s" \
+                % dtopo_fname
+    else:
+        print "Using Okada model to create %s " % dtopo_fname
+
+        # Needed for extent of dtopo file:
+        xlower = -0.4
+        xupper = 0.6
+        ylower = -0.4
+        yupper = 0.4
+
+        # number of grid points in dtopo file
+        mx = 121
+        my = 81
+
+        # Create dtopo_params dictionary with parameters for dtopo file: 
+        dtopo_params = {}
+        dtopo_params['fname'] = dtopo_fname
+        dtopo_params['faulttype'] = 'static'
+        dtopo_params['dtopotype'] = 3
+        dtopo_params['mx'] = mx
+        dtopo_params['my'] = my
+        dtopo_params['xlower'] = xlower
+        dtopo_params['xupper'] = xupper
+        dtopo_params['ylower'] = ylower
+        dtopo_params['yupper'] = yupper
+        dtopo_params['t0'] = 0.
+        dtopo_params['tfinal'] = 100.
+        dtopo_params['ntimes'] = 5
+
+        dtopo = dtopotools.make_dtopo_from_subfaults(subfaults, dtopo_params)
+
+        if plotfig:
+            figure(plotfig)
+            x = dtopo.x
+            y = dtopo.y
+            dz_final = dtopo.dz_list[-1]
+            dtopotools.plot_dz_colors(x,y,dz_final,cmax_dz=16,dz_interval=1)
+
+        return dtopo
 
 if __name__=='__main__':
     maketopo()
     #maketopo2()
-    #makedtopo()
+    make_dtopo_dtopo1()
