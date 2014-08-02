7452b43957523b75f1cd8fbd5e7ed165e6757eee
Kyle Mandli <kyle.mandli@gmail.com>
2014-08-01 21:10:47 -0500

    Move distance related functions to util module

--- src/python/geoclaw/util.py
+++ src/python/geoclaw/util.py
@@ -6,6 +6,21 @@ import os
 import urllib2
 import tarfile
 
+import numpy
+
+import clawpack.geoclaw.data
+
+# ==============================================================================
+#  Constants
+# ==============================================================================
+from data import Rearth
+DEG2RAD = numpy.pi / 180.0
+RAD2DEG = 180.0 / numpy.pi
+LAT2METER = clawpack.geoclaw.data.Rearth * DEG2RAD
+
+# ======================
+#  Remote file handling
+# ======================
 def strip_archive_extensions(path, extensions=["tar", "tgz", "bz2", "gz"]):
     r"""
     Strip off archive extensions defined in *extensions* list.
@@ -93,3 +108,106 @@ def get_remote_file(url, output_dir=None, file_name=None, force=False,
         return None
 
     return unarchived_output_path
+
+
+# ==============================================================================
+#  Functions for calculating Distances
+# ==============================================================================
+def dms2decimal(d,m,s,coord='N'):
+    r"""Convert coordinates in (degrees, minutes, seconds) to decimal form.  
+    
+    If coord == 'S' or coord == 'W' then value is negated too.
+    Example: 
+        >>> topotools.dms2decimal(7,30,36,'W')
+        -7.51
+    (Note that you might want to add 360 to resulting W coordinate
+    if using E coordinates everywhere in a computation spanning date line.)
+
+    returns float
+
+    """
+
+    deg = d + m / 60.0 + s / 3600.0
+    if coord in ['S','W']:
+        deg = -deg
+
+    return deg
+
+
+def dist_latlong2meters(dx, dy, latitude=0.0):
+    """Convert distance from degrees longitude-latitude to meters.
+
+    Takes the distance described by *dx* and *dy* in degrees and converts it into
+    distances in meters.
+
+    returns (float, float) 
+
+    """
+
+    dym = Rearth * DEG2RAD * dy
+    dxm = Rearth * numpy.cos(latitude * DEG2RAD) * dx * DEG2RAD
+
+    return dxm,dym
+
+
+def dist_meters2latlong(dx, dy, latitude=0.0):
+    """Convert distance from meters to degrees of longitude-latitude.
+
+    Takes the distance described by *dx* and *dy* in meters and converts it into
+    distances in the longitudinal and latitudinal directions in degrees.  
+
+    returns (float, float)
+
+    """
+
+    dxd = dx / (Rearth * numpy.cos(latitude * DEG2RAD)) * RAD2DEG
+    dyd = dy * RAD2DEG / Rearth
+
+    return dxd, dyd
+
+
+def haversine(x, y, units='degrees'):
+    r"""Compute the great circle distance on the earth between points x and y.
+
+
+    """
+    if units == 'degrees':
+        # convert to radians:
+        x *= DEG2RAD
+        y *= DEG2RAD
+
+    delta = [x[0] - y[0], x[1] - y[1]]
+
+    # angle subtended by two points, using Haversine formula:
+    dsigma = 2.0 * numpy.arcsin( numpy.sqrt( numpy.sin(0.5 * delta[1])**2   \
+            + numpy.cos(x[1]) * numpy.cos(y[1]) * numpy.sin(0.5 * delta[0])**2))
+
+    # alternative formula that may have more rounding error:
+    #dsigma2 = arccos(sin(y1)*sin(y2)+ cos(y1)*cos(y2)*cos(dx))
+    #print "max diff in dsigma: ", abs(dsigma-dsigma2).max()
+
+    return Rearth * dsigma
+    
+
+def inv_haversine(d,x1,y1,y2,Rsphere=Rearth,units='degrees'):
+    r"""Invert the Haversine function to find dx given a distance and point.
+
+
+    Invert the haversine function to find dx given distance d and (x1,y1) and y2.
+    The corresponding x2 can be x1+dx or x1-dx.
+    May return NaN if no solution.
+    """
+
+    if units=='degrees':
+        # convert to radians:
+        x1 = x1 * RAD2DEG
+        y1 = y1 * RAD2DEG
+        y2 = y2 * RAD2DEG
+    elif units != 'radians':
+        raise Exception("unrecognized units")
+    dsigma = d / Rsphere
+    cos_dsigma = (numpy.cos(dsigma) - numpy.sin(y1)*numpy.sin(y2)) / (numpy.cos(y1)*numpy.cos(y2))
+    dx = numpy.arccos(cos_dsigma)
+    if units=='degrees':
+        dx = dx * RAD2DEG
+    return dx
