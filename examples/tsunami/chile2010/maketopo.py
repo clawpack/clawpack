9e11e410edfa1daa034398ff6f27d7ceae8b3a2f
Randy LeVeque <rjl@uw.edu>
2014-08-22 10:57:07 -0700

    Fixed maketopo.py file in chile2010 example for latest tools and to also plot if requested.

--- examples/tsunami/chile2010/maketopo.py
+++ examples/tsunami/chile2010/maketopo.py
@@ -1,8 +1,11 @@
 """
-Create topo files needed for this example:
+Create topo and dtopo files needed for this example:
     etopo10min120W60W60S0S.asc        download from GeoClaw topo repository
-    usgs100227.tt1                    create using Okada model and .cfg file
+    dtopo_usgs100227.tt3              create using Okada model 
+Prior to Clawpack 5.2.1, the fault parameters we specified in a .cfg file,
+but now they are explicit below.
     
+Call functions with makeplots==True to create plots of topo, slip, and dtopo.
 """
 
 import os,sys
@@ -15,7 +18,7 @@ except:
 # Scratch directory for storing topo and dtopo files:
 scratch_dir = os.path.join(CLAW, 'geoclaw', 'scratch')
 
-def get_topo():
+def get_topo(makeplots=False):
     """
     Retrieve the topo file from the GeoClaw repository.
     """
@@ -25,8 +28,17 @@ def get_topo():
     util.get_remote_file(url, output_dir=scratch_dir, file_name=topo_fname,
             verbose=True)
 
+    if makeplots:
+        from matplotlib import pyplot as plt
+        topo = topotools.Topography(topo_fname, topo_type=2)
+        topo.plot()
+        fname = os.path.splitext(topo_fname)[0] + '.png'
+        plt.savefig(fname)
+        print "Created ",fname
+
+
     
-def make_dtopo():
+def make_dtopo(makeplots=False):
     """
     Create dtopo data file for deformation of sea floor due to earthquake.
     Uses the Okada model with fault parameters and mesh specified below.
@@ -36,49 +48,62 @@ def make_dtopo():
 
     dtopo_fname = os.path.join(scratch_dir, "dtopo_usgs100227.tt3")
 
+    # Specify subfault parameters for this simple fault model consisting
+    # of a single subfault:
+
+    usgs_subfault = dtopotools.SubFault()
+    usgs_subfault.strike = 16.
+    usgs_subfault.length = 450.e3
+    usgs_subfault.width = 100.e3
+    usgs_subfault.depth = 35.e3
+    usgs_subfault.slip = 15.
+    usgs_subfault.rake = 104.
+    usgs_subfault.dip = 14.
+    usgs_subfault.longitude = -72.668
+    usgs_subfault.latitude = -35.826
+    usgs_subfault.coordinate_specification = "top center"
+
+    fault = dtopotools.Fault()
+    fault.subfaults = [usgs_subfault]
+
+    print "Mw = ",fault.Mw()
+
     if os.path.exists(dtopo_fname):
-        print "*** Not regenerating dtopo file (already exists): %s" % dtopo_fname
+        print "*** Not regenerating dtopo file (already exists): %s" \
+                    % dtopo_fname
     else:
         print "Using Okada model to create dtopo file"
 
-        usgs_subfault = dtopotools.SubFault()
-        usgs_subfault.strike = 16.
-        usgs_subfault.length = 450.e3
-        usgs_subfault.width = 100.e3
-        usgs_subfault.depth = 35.e3
-        usgs_subfault.slip = 15.
-        usgs_subfault.rake = 104.
-        usgs_subfault.dip = 14.
-        usgs_subfault.longitude = -72.668
-        usgs_subfault.latitude = -35.826
-        usgs_subfault.coordinate_specification = "top center"
-
         x = numpy.linspace(-77, -67, 100)
         y = numpy.linspace(-40, -30, 100)
-        times = [0., 1.]
+        times = [1.]
 
-        fault = dtopotools.Fault()
-        fault.subfaults = [usgs_subfault]
         fault.create_dtopography(x,y,times)
         dtopo = fault.dtopo
-        dtopo.write(dtopo_fname, 3)
+        dtopo.write(dtopo_fname, dtopo_type=3)
 
-        print "Mw = ",fault.Mw()
 
-        if 1:
-            from matplotlib import pyplot as plt
-            plt.figure(figsize=(12,7))
-            ax1 = plt.subplot(121)
-            ax2 = plt.subplot(122)
-            fault.plot_subfaults(axes=ax1,slip_color=True)
-            ax1.set_xlim(x.min(),x.max())
-            ax1.set_ylim(y.min(),y.max())
-            dtopo.plot_dz_colors(1.,axes=ax2)
-            fname = 'dtopo_usgs100227.png'
-            plt.savefig(fname)
-            print "Created ",fname
+    if makeplots:
+        from matplotlib import pyplot as plt
+        if fault.dtopo is None:
+            # read in the pre-existing file:
+            print "Reading in dtopo file..."
+            dtopo = dtopotools.DTopography()
+            dtopo.read(dtopo_fname, dtopo_type=3)
+            x = dtopo.x
+            y = dtopo.y
+        plt.figure(figsize=(12,7))
+        ax1 = plt.subplot(121)
+        ax2 = plt.subplot(122)
+        fault.plot_subfaults(axes=ax1,slip_color=True)
+        ax1.set_xlim(x.min(),x.max())
+        ax1.set_ylim(y.min(),y.max())
+        dtopo.plot_dz_colors(1.,axes=ax2)
+        fname = os.path.splitext(dtopo_fname)[0] + '.png'
+        plt.savefig(fname)
+        print "Created ",fname
 
 
 if __name__=='__main__':
-    get_topo()
-    make_dtopo()
+    get_topo(False)
+    make_dtopo(False)
