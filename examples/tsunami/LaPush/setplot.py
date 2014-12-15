a157195db466fb329211b0cee5651b5c69a492b5
Donna <donna.calhoun@gmail.com>
2014-12-14 19:30:10 -0700

    Increased resolution; suppressed printing of html files

--- examples/tsunami/LaPush/setplot.py
+++ examples/tsunami/LaPush/setplot.py
@@ -84,7 +84,7 @@ def setplot(plotdata):
     #-----------------------------------------
     plotfigure = plotdata.new_plotfigure(name='Pacific', figno=0)
     plotfigure.kwargs = {'figsize': (8,10)}
-    #plotfigure.show = False
+    plotfigure.show = False
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
@@ -157,7 +157,7 @@ def setplot(plotdata):
     # Figure for zoom1
     #-----------------------------------------
     plotfigure = plotdata.new_plotfigure(name="Olympics", figno=11)
-    #plotfigure.show = False
+    plotfigure.show = False
     plotfigure.kwargs = {'figsize': (10,9)}
 
     # Set up for axes in this figure:
@@ -193,7 +193,7 @@ def setplot(plotdata):
     # Figure for zoom2
     #-----------------------------------------
     plotfigure = plotdata.new_plotfigure(name="LaPush", figno=12)
-    #plotfigure.show = True
+    plotfigure.show = False
     plotfigure.kwargs = {'figsize': (10,9)}
 
     # Set up for axes in this figure:
@@ -266,7 +266,7 @@ def setplot(plotdata):
     plotfigure = plotdata.new_plotfigure(name='kml_figure',figno=1)
     plotfigure.show = True   # Don't show this file in the html version
     plotfigure.use_for_kml = True
-    plotfigure.kml_dpi = 200
+    plotfigure.kml_dpi = 1600
     plotfigure.kml_xlimits = [-124.68, -124.55]
     plotfigure.kml_ylimits = [47.86, 47.96]
     # plotfigure.kml_xlimits = [-126,-124]
