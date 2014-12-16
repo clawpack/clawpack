cf0c5f7ecac5813cecac6c0b335c6dc985bea1fa
Donna <donna.calhoun@gmail.com>
2014-12-16 11:58:33 -0700

    Added overwrite option to gdalwarp to suppress annoying warning

--- examples/tsunami/chile2010/run_gdal.sh
+++ examples/tsunami/chile2010/run_gdal.sh
@@ -1,21 +1,24 @@
 #!/bin/tcsh
 
-set file = frame00$1fig1
+set file = frame000$1fig1
 set ext = png
 
 setenv GDAL_DATA /opt/local/share/gdal
 
-gdalinfo $file.$ext
+# gdalinfo $file.$ext
 
 gdal_translate -of VRT \
     -a_srs EPSG:4326 \
     -gcp 0        0 -120   0      \
     -gcp 2400     0  -60   0      \
-    -gcp 2400  2400  -60 -60  -90 \
-    $file.$ext $file.vrt
+    -gcp 2400  2400  -60 -60    \
+    -90 \
+    $file.$ext "$file"_tmp.vrt
 
- gdalwarp -of VRT \
-     -t_srs EPSG:4326 \
-     $file.vrt "$file".vrt
+gdalwarp -of VRT \
+       -t_srs EPSG:4326 \
+       -overwrite \
+	"$file"_tmp.vrt $file.vrt
 
-gdal2tiles.py --profile=geodetic  --force-kml --resampling=near "$file".vrt
+
+gdal2tiles.py --profile=geodetic  --force-kml --resampling=near $file.vrt
