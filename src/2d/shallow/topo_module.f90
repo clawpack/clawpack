00a24024bdf8d6356a81a50d86d359e3b118d233
Randy LeVeque <rjl@uw.edu>
2015-03-24 16:12:09 -0600

    modified topo_module.f90 so (1) topo_type 2 or 3 can have either cellsize a single value for dx=dy or two different values, and (2) descriptions strings like cellsize can come before or after values, to avoid need to run swapheader on topo data downloaded from NGDC

--- src/2d/shallow/topo_module.f90
+++ src/2d/shallow/topo_module.f90
@@ -558,9 +558,11 @@ contains
 
         ! Local
         integer, parameter :: iunit = 19
-        integer :: topo_size, status
+        integer :: topo_size, status, n
         real(kind=8) :: x,y,z,nodata_value
         logical :: found_file
+        real(kind=8) :: values(10)
+        character(len=80) :: str
 
         inquire(file=fname,exist=found_file)
         if (.not. found_file) then
@@ -612,13 +614,32 @@ contains
 
             ! ASCII file with header followed by z data
             case(2:3)
-                read(iunit,*) mx
-                read(iunit,*) my
-                read(iunit,*) xll
-                read(iunit,*) yll
-                read(iunit,*) dx
-                read(iunit,*) nodata_value
-                dy = dx
+                read(iunit,'(a)') str
+                call parse_values(str, n, values)
+                mx = nint(values(1))
+
+                read(iunit,'(a)') str
+                call parse_values(str, n, values)
+                my = nint(values(1))
+
+                read(iunit,'(a)') str
+                call parse_values(str, n, values)
+                xll = values(1)
+
+                read(iunit,'(a)') str
+                call parse_values(str, n, values)
+                yll = values(1)
+
+                read(iunit,'(a)') str
+                call parse_values(str, n, values)
+                dx = values(1)
+                if (n == 2) then
+                    dy = values(2)
+                  else
+                    dy = dx
+                  endif
+                call parse_values(str, n, values)
+                nodata_value = values(1)
                 xhi = xll + (mx-1)*dx
                 yhi = yll + (my-1)*dy
 
