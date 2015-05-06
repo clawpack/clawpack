2825454bc0ca22081c48b01af987b433bddebd13
donnaaboise <donna.calhoun@gmail.com>
2015-05-06 10:11:11 -0600

    fixed aliasing issue

--- examples/tsunami/LaPush/setplot.py
+++ examples/tsunami/LaPush/setplot.py
@@ -39,13 +39,15 @@ def setplot(plotdata):
 
     """
 
+    # This file has been modifid significantly to illustrate Google Earth plotting
+    # tools.
 
     from clawpack.visclaw import colormaps, geoplot
 
     plotdata.clearfigures()  # clear any old figures,axes,items dat
     plotdata.format = 'binary'
 
-    clim_ocean = 0.2
+    clim_ocean = 8.0
     clim_coast = 8.0
 
     sealevel = 0.  # Level of tide in run relative to MHW
@@ -54,49 +56,36 @@ def setplot(plotdata):
     cmax_coast = clim_coast + sealevel
     cmin_coast = -clim_coast + sealevel
 
-
-    # To plot gauge locations on pcolor or contour plot, use this as
-    # an afteraxis function:
-
-    def addgauges(current_data):
-        from clawpack.visclaw import gaugetools
-        gaugetools.plot_gauge_locations(current_data.plotdata, \
-             gaugenos=[0,1,2,4,5,6,7], format_string='ko', add_labels=True)
-
-    def timeformat(t):
-        from numpy import mod
-        hours = int(t/3600.)
-        tmin = mod(t,3600.)
-        min = int(tmin/60.)
-        sec = int(mod(tmin,60.))
-        timestr = '%s:%s:%s' % (hours,str(min).zfill(2),str(sec).zfill(2))
-        return timestr
-
-    def title_hours(current_data):
-        from pylab import title
-        t = current_data.t
-        timestr = timeformat(t)
-        title('%s after earthquake' % timestr)
-
     # ---------------------------------------
     # Some KML info
     # ---------------------------------------
     plotdata.kml_index_fname = "LaPush"     # Name for .kmz and .kml files.
     plotdata.kml_name = "LaPush"
     plotdata.kml_publish = 'http://math.boisestate.edu/~calhoun/visclaw/GoogleEarth/kmz/'
-    kml_dpi = 400
 
 
     light_green_a = [0.0,1.0,1.0,1.0];
     transparent = [0.0,0.0,0.0,0.0]
     red_a = [1.0,0.0,0.0,1.0]
-    mycmap = colormaps.make_colormap({-1:light_green_a,
-                                      0.0:transparent,
-                                      1:red_a})
-    def kml_colorbar(filename):
+    lightblue = "#4E6498"  # Matches region fill color for comp. domain
+
+    lightblue_cmap = colormaps.make_colormap({-1:light_green_a,
+                                              0.0:lightblue,
+                                              1:red_a})
+
+    transparent_cmap = colormaps.make_colormap({-1:light_green_a,
+                                                0.0:transparent,
+                                                1:red_a})
+
+    def cbar_transparent(filename):
         geoplot.kml_build_colorbar(filename,
-                                   mycmap,
-                                   cmin_ocean,cmax_ocean)
+                                   transparent_cmap,
+                                   cmin_coast,cmax_coast)
+
+    def cbar_lightblue(filename):
+        geoplot.kml_build_colorbar(filename,
+                                   lightblue_cmap,
+                                   cmin_coast,cmax_coast)
 
 
 
@@ -104,288 +93,95 @@ def setplot(plotdata):
     # Figure for big area
     #-----------------------------------------
     plotfigure = plotdata.new_plotfigure(name='Pacific', figno=0)
-    plotfigure.kwargs = {'figsize': (8,10)}
     plotfigure.show = True
 
     plotfigure.use_for_kml = True
     plotfigure.kml_use_for_initial_view = True
 
-    plotfigure.kml_xlimits = [-126, -124]
-    plotfigure.kml_ylimits = [47,49]
+    plotfigure.kml_xlimits = [-126.0, -124.0]
+    plotfigure.kml_ylimits = [47.0,49.0]
 
-    plotfigure.kml_dpi = kml_dpi
-    plotfigure.kml_tile_images = False
+    # Resolution : Use num_cells and refinement factors to figure out
+    # correct resolution.
+    # Refinement factors : [10,6,16]; maxlevel = 4
+    rcl = 1   # resolution of coarsest level in this zoom;  rcl*figsize = num_cells
+    plotfigure.kml_dpi = rcl*10*6   # Resolve levels 2,3
+    plotfigure.kml_figsize = [12.0,12.0]
+    plotfigure.kml_tile_images = True
 
-    # Set up for axes in this figure:
+    plotfigure.kml_colorbar = cbar_transparent
+
+    # Plot sea surface height
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.title = 'Pacific'
-    plotaxes.scaled = False
-    plotaxes.xlimits = [-126, -124]
-    plotaxes.ylimits = [47,49]
-
-    def aa(current_data):
-        from pylab import ticklabel_format, xticks, gca, cos, pi, savefig
-        title_hours(current_data)
-        ticklabel_format(format='plain',useOffset=False)
-        xticks(rotation=20)
-        a = gca()
-        a.set_aspect(1./cos(48*pi/180.))
-    #plotaxes.afteraxes = aa
-
-    # Water
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = geoplot.surface_or_depth
-
-
-    plotfigure.kml_colorbar = kml_colorbar
-
-
-
-    plotitem.imshow_cmap = mycmap
-    plotitem.imshow_cmin = cmin_ocean
-    plotitem.imshow_cmax = cmax_ocean
-    plotitem.add_colorbar = True
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-
-    # Land
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
-    plotitem.plot_var = geoplot.land
-    plotitem.show = False
-    plotitem.imshow_cmap = geoplot.land_colors
-    plotitem.imshow_cmin = 0.0
-    plotitem.imshow_cmax = 100.0
-    plotitem.add_colorbar = False
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-    #plotaxes.afteraxes = addgauges
-
-    # Add contour lines of bathymetry:
-    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
-    plotitem.show = False
-    plotitem.plot_var = geoplot.topo
-    from numpy import arange, linspace
-    plotitem.contour_levels = linspace(-6000,0,7)
-    plotitem.amr_contour_colors = ['g']  # color on each level
-    plotitem.kwargs = {'linestyles':'solid'}
-    plotitem.amr_contour_show = [0,0,1,0]  # show contours only on finest level
-    plotitem.celledges_show = 0
-    plotitem.patchedges_show = 0
-
-    # Add contour lines of topography:
-    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
-    plotitem.show = False
-    plotitem.plot_var = geoplot.topo
-    from numpy import arange, linspace
-    plotitem.contour_levels = arange(0., 11., 1.)
-    plotitem.amr_contour_colors = ['g']  # color on each level
-    plotitem.kwargs = {'linestyles':'solid'}
-    plotitem.amr_contour_show = [0,0,0,1]  # show contours only on finest level
-    plotitem.celledges_show = 0
-    plotitem.patchedges_show = 0
-
+    plotitem.pcolor_cmap = transparent_cmap
+    plotitem.pcolor_cmin = cmin_ocean
+    plotitem.pcolor_cmax = cmax_ocean
 
     #-----------------------------------------
     # Figure for zoom1
     #-----------------------------------------
-    plotfigure = plotdata.new_plotfigure(name="Olympics", figno=11)
+    plotfigure = plotdata.new_plotfigure(name="Olympics", figno=1)
     plotfigure.show = True
-    plotfigure.kwargs = {'figsize': (10,9)}
 
     plotfigure.use_for_kml = True
     plotfigure.kml_use_for_initial_view = False
 
-    plotfigure.kml_xlimits = [-125, -124.3]
+    # Limits should align with coarse grid.
+    plotfigure.kml_xlimits = [-125.0, -124.0]
     plotfigure.kml_ylimits = [47.5,48.5]
 
-    plotfigure.kml_dpi = kml_dpi
+    # rcl : Resolution of the coarsest level in this zoom, where
+    #       rcl*figsize == number of coarsest level cells in each
+    #       direction.
+    rcl = 10
+    plotfigure.kml_dpi = rcl*6    # Resolve level 3
+    plotfigure.kml_figsize = [6.0,6.0]
     plotfigure.kml_tile_images = True
 
-    plotfigure.kml_colorbar = kml_colorbar
-
-    # Set up for axes in this figure:
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.scaled = False
-    plotaxes.xlimits = [-125, -124.3]
-    plotaxes.ylimits = [47.5, 48.5]
-    #plotaxes.afteraxes = aa
+    plotfigure.kml_colorbar = cbar_transparent
 
     # Water
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotaxes = plotfigure.new_plotaxes()
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = geoplot.surface_or_depth
-    plotitem.imshow_cmap = mycmap
-    plotitem.imshow_cmin = cmin_coast
-    plotitem.imshow_cmax = cmax_coast
-    plotitem.add_colorbar = True
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-
-    # Land
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
-    plotitem.show = False
-    plotitem.plot_var = geoplot.land
-    plotitem.imshow_cmap = geoplot.land_colors
-    plotitem.imshow_cmin = 0.0
-    plotitem.imshow_cmax = 100.0
-    plotitem.add_colorbar = False
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-
-
+    plotitem.pcolor_cmap = transparent_cmap
+    plotitem.pcolor_cmin = cmin_coast
+    plotitem.pcolor_cmax = cmax_coast
 
     #-----------------------------------------
     # Figure for zoom2
     #-----------------------------------------
-    plotfigure = plotdata.new_plotfigure(name="LaPush", figno=12)
+    plotfigure = plotdata.new_plotfigure(name="LaPush", figno=2)
     plotfigure.show = True
-    plotfigure.kwargs = {'figsize': (10,9)}
 
     plotfigure.use_for_kml = True
     plotfigure.kml_use_for_initial_view = False
 
-    plotfigure.kml_xlimits = [-124.68, -124.55]
-    plotfigure.kml_ylimits = [47.86, 47.96]
+    # Should align with a [720x720] grid in larger computational domain.
+    plotfigure.kml_xlimits = [-124.7, -124.55]
+    plotfigure.kml_ylimits = [47.8, 47.95]
 
-    plotfigure.kml_dpi = kml_dpi
+    # Resolution
+    # rcl : Resolution of the coarsest level in this zoom, where
+    #       rcl*figsize == number of coarsest level cells in each
+    #       direction.
+    rcl = 10   # figsize*rcl = number of coarsest level cells in this zoom
+    plotfigure.kml_dpi = rcl*16  # Resolve finest level.
+    plotfigure.kml_figsize = [5.4,5.4]
     plotfigure.kml_tile_images = True
 
-    plotfigure.kml_colorbar = kml_colorbar
+    plotfigure.kml_colorbar = cbar_lightblue
 
 
-    # Set up for axes in this figure:
+    # Water
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.title = "LaPush"
-    plotaxes.scaled = False
-    plotaxes.xlimits = [-124.68, -124.55]
-    plotaxes.ylimits = [47.86, 47.96]
-    #plotaxes.afteraxes = aa
-
-    # Water
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
-    plotitem.plot_var = geoplot.surface_or_depth
-    plotitem.imshow_cmap = mycmap
-    plotitem.imshow_cmin = cmin_coast
-    plotitem.imshow_cmax = cmax_coast
-    plotitem.add_colorbar = True
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-
-    # Land
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
-    plotitem.plot_var = geoplot.land
-    plotitem.show = False
-    plotitem.imshow_cmap = geoplot.land_colors
-    plotitem.imshow_cmin = 0.0
-    plotitem.imshow_cmax = 100.0
-    plotitem.add_colorbar = False
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-
-
-    #-----------------------------------------
-    # Figure for fixed grid area with GE image
-    #-----------------------------------------
-    plotfigure = plotdata.new_plotfigure(name="fgmax", figno=13)
-    plotfigure.show = False
-    plotfigure.kwargs = {'figsize': (12,8)}
-
-    plotfigure.use_for_kml = True
-    plotfigure.kml_use_for_initial_view = False
-
-    plotfigure.kml_xlimits = [-124.68, -124.55]
-    plotfigure.kml_ylimits = [47.86, 47.96]
-
-    plotfigure.kml_dpi = kml_dpi
-    plotfigure.kml_tile_images = True
-
-    plotfigure.kml_colorbar = kml_colorbar
-
-    # Set up for axes in this figure:
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.title = "LaPush"
-    plotaxes.scaled = False
-    plotaxes.xlimits = [-124.68, -124.55]
-    plotaxes.ylimits = [47.86, 47.96]
-    #plotaxes.afteraxes = aa
-
-    # Water
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
-    plotitem.plot_var = geoplot.surface_or_depth
-    plotitem.imshow_cmap = mycmap
-    plotitem.imshow_cmin = cmin_coast
-    plotitem.imshow_cmax = cmax_coast
-    plotitem.add_colorbar = True
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-
-    # Land
-    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
-    plotitem.plot_var = geoplot.land
-    plotitem.imshow_cmap = geoplot.land_colors
-    plotitem.imshow_cmin = 0.0
-    plotitem.imshow_cmax = 100.0
-    plotitem.add_colorbar = False
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.amr_patchedges_show = [0]
-
-    #-------------------------------------------------------------------
-    # Figure for KML files
-    #--------------------------------------------------------------------
-
-    plotfigure = plotdata.new_plotfigure(name='LaPush 2',figno=14)
-    plotfigure.show = True   # Don't show this file in the html version
-
-
-    plotfigure.use_for_kml = True
-    plotfigure.kml_use_for_initial_view = False
-
-
-    plotfigure.kml_xlimits = [-124.68, -124.55]
-    plotfigure.kml_ylimits = [47.86, 47.96]
-
-
-    plotfigure.kml_dpi = kml_dpi
-    plotfigure.kml_tile_images = True
-
-
-    plotfigure.kml_colorbar = kml_colorbar
-
-    # Set up for axes in this figure:
-    plotaxes = plotfigure.new_plotaxes('kml')
-    plotaxes.xlimits =  [-124.68, -124.55]
-    plotaxes.ylimits = [47.86, 47.96]
-    plotaxes.scaled = True
-
-    # Water
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = geoplot.surface_or_depth
-    # plotitem.plot_var = geoplot.surface
-    plotitem.pcolor_cmap = mycmap
+    plotitem.pcolor_cmap = lightblue_cmap
     plotitem.pcolor_cmin = cmin_coast
     plotitem.pcolor_cmax = cmax_coast
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.patchedges_show = 0
-
-
-
-#     #-----------------------------------------
-#     # Figures for gauges
-#     #-----------------------------------------
-#     plotfigure = plotdata.new_plotfigure(name='gauge plot', figno=300, \
-#                     type='each_gauge')
-#     #plotfigure.clf_each_gauge = False
-#
-#     # Set up for axes in this figure:
-#     plotaxes = plotfigure.new_plotaxes()
-#     plotaxes.ylimits = [-1,10]
-#     plotaxes.title = 'Flow depth'
-#
-#     # Plot depth as blue curve:
-#     plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-#     plotitem.plot_var = 0
-#     plotitem.plotstyle = 'b-'
-#
-
 
     #-----------------------------------------
 
@@ -395,7 +191,7 @@ def setplot(plotdata):
     plotdata.printfigs = True                # print figures
     plotdata.print_format = 'png'            # file format
     plotdata.print_framenos = 'all'          # list of frames to print
-    plotdata.print_fignos = [0,11,12]           # list of figures to print
+    plotdata.print_fignos = [0,1,2]           # list of figures to print
     plotdata.print_gaugenos = 'all'          # list of gauges to print
     plotdata.html = True                     # create html files of plots?
     plotdata.html_homelink = '../README.html'   # pointer for top of index
