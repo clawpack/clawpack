18c17a381ec52d7a6bd6a1ab00e09912d7967f6c
donnaaboise <donna.calhoun@gmail.com>
2015-05-07 11:36:43 -0600

    Updated Chile 2010 setplot.py . Differs from master only in the added KML section

--- examples/tsunami/chile2010/setplot.py
+++ examples/tsunami/chile2010/setplot.py
@@ -34,7 +34,6 @@ def setplot(plotdata):
 
     plotdata.clearfigures()  # clear any old figures,axes,items data
 
-    plotdata.verbose = False
 
     # To plot gauge locations on pcolor or contour plot, use this as
     # an afteraxis function:
@@ -45,17 +44,6 @@ def setplot(plotdata):
              gaugenos='all', format_string='ko', add_labels=True)
 
 
-
-    #-----------------------------------------
-    # Some global kml flags
-    #-----------------------------------------
-    plotdata.kml_name = "Chile 2010"
-    plotdata.kml_starttime = [2010,2,27,6,34,0]  # Time of event in UTC [None]
-    plotdata.kml_tz_offset = 3    # Time zone offset (in hours) of event. [None]
-
-    plotdata.kml_index_fname = "Chile_2010"  # name for .kmz and .kml files ["_GoogleEarth"]
-    plotdata.kml_publish = 'http://www.yourdomainname.here'
-
     #-----------------------------------------
     # Figure for surface
     #-----------------------------------------
@@ -63,7 +51,7 @@ def setplot(plotdata):
 
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes('pcolor')
-    plotaxes.title = ''
+    plotaxes.title = 'Surface'
     plotaxes.scaled = True
 
     def fixup(current_data):
@@ -78,42 +66,59 @@ def setplot(plotdata):
 
     # Water
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.plot_var = geoplot.surface
+    #plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
     plotitem.pcolor_cmap = geoplot.tsunami_colormap
     plotitem.pcolor_cmin = -0.2
     plotitem.pcolor_cmax = 0.2
     plotitem.add_colorbar = True
     plotitem.amr_celledges_show = [0,0,0]
-    plotitem.patchedges_show = 0
-    plotaxes.xlimits = [-120,-60]
-    plotaxes.ylimits = [-60,0]
+    plotitem.patchedges_show = 1
 
     # Land
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
-    plotitem.show = True
     plotitem.plot_var = geoplot.land
     plotitem.pcolor_cmap = geoplot.land_colors
     plotitem.pcolor_cmin = 0.0
     plotitem.pcolor_cmax = 100.0
-    plotitem.add_colorbar = True
-    plotitem.amr_celledges_show = [0,0,0]
-    plotitem.patchedges_show = 0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [1,1,0]
+    plotitem.patchedges_show = 1
+    plotaxes.xlimits = [-120,-60]
+    plotaxes.ylimits = [-60,0]
 
     # add contour lines of bathy if desired:
     plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
-    plotitem.show = True
+    plotitem.show = False
     plotitem.plot_var = geoplot.topo
     plotitem.contour_levels = linspace(-3000,-3000,1)
     plotitem.amr_contour_colors = ['y']  # color on each level
     plotitem.kwargs = {'linestyles':'solid','linewidths':2}
     plotitem.amr_contour_show = [1,0,0]
     plotitem.celledges_show = 0
-    plotitem.patchedges_show = [1,1,1]
+    plotitem.patchedges_show = 0
 
 
     #-----------------------------------------------------------
-    # Figure for KML files
+    # For plotting in Google Earth
     #----------------------------------------------------------
+
+    # Some global kml flags
+    plotdata.kml_name = "Chile 2010"
+    plotdata.kml_starttime = [2010,2,27,6,34,0]  # Time of event in UTC [None]
+    plotdata.kml_tz_offset = 3    # Time zone offset (in hours) of event. [None]
+
+    plotdata.kml_index_fname = "Chile_2010"  # name for .kmz and .kml files ["_GoogleEarth"]
+    plotdata.kml_publish = 'http://www.yourserver.edu/path/to/kmz/files'
+
+    # Used for both figures
+    cmin = -0.2
+    cmax = 0.2
+    cmap = geoplot.googleearth_transparent
+
+    #-----------------------------------------
+    # Sea surface (Google Earth)
+    #-----------------------------------------
     plotfigure = plotdata.new_plotfigure(name='Sea Surface',figno=1)
     plotfigure.show = True
 
@@ -131,13 +136,9 @@ def setplot(plotdata):
     rcl = 1    # rcl*figsize = num_cells
     plotfigure.kml_figsize = [30.0,30.0]
     plotfigure.kml_dpi = rcl*2*6         # Resolve all three levels
-    plotfigure.kml_tile_images = True    # Tile images for faster loading.  Requires GDAL [False]
-
+    plotfigure.kml_tile_images = False    # Tile images for faster loading.  Requires GDAL [False]
 
     # Water
-    cmin = -0.2
-    cmax = 0.2
-    cmap = geoplot.googleearth_transparent
     plotaxes = plotfigure.new_plotaxes('kml')
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
     plotitem.plot_var = geoplot.surface_or_depth
@@ -151,7 +152,7 @@ def setplot(plotdata):
     plotfigure.kml_colorbar = kml_colorbar
 
     #-----------------------------------------------------------
-    # Figure for KML files (zoom)
+    # Zoomed region (GoogleEarth)
     #----------------------------------------------------------
     plotfigure = plotdata.new_plotfigure(name='Sea Surface (zoom)',figno=2)
     plotfigure.show = True
@@ -167,13 +168,7 @@ def setplot(plotdata):
     # Resolution
     rcl = 10    # Over-resolve the coarsest level
     plotfigure.kml_dpi = rcl*2*6       # Resolve all three levels
-    plotfigure.kml_tile_images = True  # Tile images for faster loading.
-
-
-    # Water
-    cmin = -0.2
-    cmax = 0.2
-    cmap = geoplot.googleearth_transparent
+    plotfigure.kml_tile_images = False  # Tile images for faster loading.
 
     plotaxes = plotfigure.new_plotaxes('kml')
     plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
@@ -181,12 +176,6 @@ def setplot(plotdata):
     plotitem.pcolor_cmap = cmap
     plotitem.pcolor_cmin = cmin
     plotitem.pcolor_cmax = cmax
-
-    def kml_colorbar(filename):
-        geoplot.kml_build_colorbar(filename,cmap,cmin,cmax)
-
-    plotfigure.kml_colorbar = kml_colorbar
-
 
     #-----------------------------------------
     # Figures for gauges
@@ -199,7 +188,7 @@ def setplot(plotdata):
     plotaxes = plotfigure.new_plotaxes()
     plotaxes.xlimits = 'auto'
     plotaxes.ylimits = 'auto'
-    # plotaxes.title = 'Surface'
+    plotaxes.title = 'Surface'
 
     # Plot surface as blue curve:
     plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
@@ -208,7 +197,7 @@ def setplot(plotdata):
 
     # Plot topo as green curve:
     plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.show = True
+    plotitem.show = False
 
     def gaugetopo(current_data):
         q = current_data.q
@@ -248,18 +237,16 @@ def setplot(plotdata):
 
     plotdata.printfigs = True                # print figures
     plotdata.print_format = 'png'            # file format
-    plotdata.print_framenos = 'all'         # list of frames to print
+    plotdata.print_framenos = 'all'          # list of frames to print
     plotdata.print_gaugenos = 'all'          # list of gauges to print
-    plotdata.print_fignos = [0,1,2,300]           # list of figures to print
+    plotdata.print_fignos = 'all'            # list of figures to print
     plotdata.html = True                     # create html files of plots?
     plotdata.html_homelink = '../README.html'   # pointer for top of index
-    plotdata.latex = False                    # create latex file of plots?
+    plotdata.latex = True                    # create latex file of plots?
     plotdata.latex_figsperline = 2           # layout of plots
     plotdata.latex_framesperline = 1         # layout of plots
     plotdata.latex_makepdf = False           # also run pdflatex?
 
-    plotdata.kml = True
-
-    # plotdata.printfigs = False
+    plotdata.kml = False
 
     return plotdata
