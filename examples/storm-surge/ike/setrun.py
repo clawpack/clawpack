50f8cac46f8ce0893240a27bb9614b03690c88cf
Kyle Mandli <kyle.mandli@gmail.com>
2016-06-15 18:42:28 -0400

    Update data locations

--- examples/storm-surge/ike/setrun.py
+++ examples/storm-surge/ike/setrun.py
@@ -127,7 +127,7 @@ def setrun(claw_pkg='geoclaw'):
         # Output nout frames at equally spaced times up to tfinal:
         # clawdata.tfinal = days2seconds(date2days('2008091400'))
         clawdata.tfinal = days2seconds(ike_landfall.days + 0.75) + ike_landfall.seconds
-        recurrence = 24
+        recurrence = 4
         clawdata.num_output_times = int((clawdata.tfinal - clawdata.t0) 
                                             * recurrence / (60**2 * 24))
 
