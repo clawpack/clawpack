ec803d642b6a94084be0526e58a5a3c3ace9acd7
Avi <aks2203@columbia.edu>
2016-09-24 17:14:40 -0400

    setrun.py wave tolorences modified.

--- examples/multi-layer/plane_wave/setrun.py
+++ examples/multi-layer/plane_wave/setrun.py
@@ -476,7 +476,7 @@ def set_multilayer(rundata):
     data.num_layers = 2
     data.rho = [0.9, 1.0]
     data.eta = [0.0,-0.6]
-    data.wave_tolerance = [1.e-4, 1.e-4]  # not the right values
+    data.wave_tolerance = [1.e-3, 1.e-2]  # not the right values
     
     # Algorithm parameters
     data.eigen_method = 2
