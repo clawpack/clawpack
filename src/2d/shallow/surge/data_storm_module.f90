4899e1e9a552a21ad1ace44365d8927769f82e07
Kyle Mandli <kyle.mandli@gmail.com>
2017-06-21 19:50:27 -0400

    Add data based storm field module

---
+++ src/2d/shallow/surge/data_storm_module.f90
@@ -0,0 +1,123 @@
+! ==============================================================================
+! model_storm_module 
+!
+! Module contains routines for constructing a wind and pressure field based on
+! the a model of the wind and pressure fields.  
+! 
+! Many of these routines are based loosely on PADCIRC version 45.12 03/17/2006
+! ==============================================================================
+!                   Copyright (C) Clawpack Developers 2017
+!  Distributed under the terms of the Berkeley Software Distribution (BSD) 
+!  license
+!                     http://www.opensource.org/licenses/
+! ==============================================================================
+module model_storm_module
+
+    implicit none
+    save
+
+    ! Model storm type definition
+    type data_storm_type
+        ! Type of input data
+        integer :: type
+
+    end type data_storm_type
+
+    abstract interface
+        subroutine set_fields(maux, mbc, mx, my, xlower, ylower,    &
+                              dx, dy, t, aux, wind_index,           &
+                              pressure_index, storm)
+            implicit none
+            integer, intent(in) :: maux,mbc,mx,my
+            real(kind=8), intent(in) :: xlower,ylower,dx,dy,t
+            type(data_storm_type), intent(in out) :: storm
+            integer, intent(in) :: wind_index, pressure_index
+            real(kind=8), intent(inout) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
+        end subroutine set_fields
+    end interface
+
+    procedure(set_fields), pointer :: set_fields
+
+contains
+
+    ! Setup routine for model storms
+    subroutine set_storm(storm_data_path, storm, log_unit)
+
+        implicit none
+
+        ! Subroutine I/O
+        character(len=*), optional :: storm_data_path
+        type(data_storm_type), intent(in out) :: storm
+        integer, intent(in) :: log_unit
+
+        stop "Data-derived storm are not yet implemented!"
+
+    end subroutine set_storm
+
+
+    ! ==========================================================================
+    !  storm_location(t,storm)
+    !    Interpolate location of hurricane in the current time interval
+    ! ==========================================================================
+    function storm_location(t,storm) result(location)
+
+        implicit none
+
+        ! Input
+        real(kind=8), intent(in) :: t
+        type(data_storm_type), intent(in out) :: storm
+
+        stop "Data-derived storm are not yet implemented!"
+
+    end function storm_location
+
+    ! ==========================================================================
+    !  storm_direction
+    !   Angle off of due north that the storm is traveling
+    ! ==========================================================================
+    real(kind=8) function storm_direction(t, storm) result(theta)
+
+        implicit none
+
+        ! Input
+        real(kind=8), intent(in) :: t
+        type(data_storm_type), intent(in) :: storm
+
+        stop "Data-derived storm are not yet implemented!"
+
+    end function storm_direction
+
+
+    ! ==========================================================================
+    !  Use the 1980 Holland model to set the storm fields
+    ! ==========================================================================
+    subroutine set_HWRF_fields(maux, mbc, mx, my, xlower, ylower,    &
+                          dx, dy, t, aux, wind_index,           &
+                          pressure_index, storm)
+
+  
+        implicit none
+
+        ! Time of the wind field requested
+        integer, intent(in) :: maux,mbc,mx,my
+        real(kind=8), intent(in) :: xlower,ylower,dx,dy,t
+
+        ! Storm description, need in out here since we may update the storm
+        ! if at next time point
+        type(data_storm_type), intent(in out) :: storm
+
+        ! Array storing wind and presure field
+        integer, intent(in) :: wind_index, pressure_index
+        real(kind=8), intent(inout) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
+
+        stop "HWRF data input is not yet implemented!"
+
+    end subroutine set_HWRF_fields
+
+end module model_storm_module
+
+
+
+
+
+
