01e524c7bc84d4b83c898abebe39446ab92f497e
Kyle Mandli <kyle.mandli@gmail.com>
2017-06-22 18:38:04 -0400

    Add doc-strings and commandline support

--- src/python/geoclaw/units.py
+++ src/python/geoclaw/units.py
@@ -8,7 +8,6 @@ converting between them.
 from __future__ import print_function
 from __future__ import absolute_import
 
-
 import sys
 
 from clawpack.geoclaw.data import LAT2METER
@@ -57,9 +56,49 @@ conversion_factor['dyne-cm'] = [1.e-7, "Dynes - Centimeter"]
 moment_units = ['N-m', 'dyne-cm']
 
 
+def units_available():
+    r"""
+    Constructs a string suitable for reading detailing the units available.
+    """
+    output = ""
+    output = "\n".join((output, "Length"))
+    for unit in length_units:
+        output = "\n".join((output, "  %s - %s" % (conversion_factor[unit][1],
+                                                   unit)))
+    output = "\n".join((output, "Pressure - Rigidity"))
+    for unit in pressure_units:
+        output = "\n".join((output, "  %s - %s" % (conversion_factor[unit][1],
+                                                   unit)))
+    output = "\n".join((output, "Speeds"))
+    for unit in speed_units:
+        output = "\n".join((output, "  %s - %s" % (conversion_factor[unit][1],
+                                                   unit)))
+    output = "\n".join((output, "Moments"))
+    for unit in moment_units:
+        output = "\n".join((output, "  %s - %s" % (conversion_factor[unit][1],
+                                                   unit)))
+    return output
+
+
 # Unit conversion function
-def convert_units(value, old_units, new_units, verbose=False):
-    r""""""
+def convert(value, old_units, new_units, verbose=False):
+    r"""Convert *value* from *old_units* to *new_units*
+
+    :Note:
+    Currently this function only handles multiplicative conversions.  The
+    reasoning behind not just returning this conversion factor as this function
+    in the future will also support more complex unit conversions, e.g.
+    converting between temperature scales.
+
+    :Input:
+     - *value* (ndarray or float) The value(s) to be converted.
+     - *old_units* (string) Type of units that value comes in as.
+     - *new_units* (string) Type of units that value should be converted to.
+     - *verbose* (bool) Verbose output (default is False)
+
+    :Output:
+     - (ndarray or float) The converted value(s)
+    """
 
     if old_units not in conversion_factor:
         raise ValueError("Units %s not found in list of supported ",
@@ -69,10 +108,23 @@ def convert_units(value, old_units, new_units, verbose=False):
         raise ValueError("Units %s not found in list of supported ",
                          "conversions." % str(new_units))
 
+    if verbose:
+        print("Convert %s %s to %s." % (value, old_units, new_units))
+
     return value * conversion_factor[old_units][0] /    \
                    conversion_factor[new_units][0]
 
 
 if __name__ == '__main__':
     # Add commandline unit conversion capability
-    pass
+    if len(sys.argv) == 4:
+        convert(float(sys.argv[1], sys.argv[2], sys.argv[3]))
+    else:
+        # Usage and available units
+        print("Usage:  Convert value in units to new units.")
+        print("  units <value> <old units> <new units>")
+        print("where <old units> and <new units> are one of the available")
+        print("units listed below.")
+        print("")
+        print("Available Units:")
+        print(units_available())
