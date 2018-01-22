c6bb4b0ed9ef9827725bafb79d500b51178d83d4
Kyle Mandli <kyle.mandli@gmail.com>
2018-01-21 20:13:40 -0500

    Change this file so it even runs
    I am not sure why this is now being used as a test but it should now
    at least work.

--- tests/topo_test.py
+++ tests/topo_test.py
@@ -10,7 +10,7 @@ import tempfile
 
 import matplotlib.pyplot as plt
 from clawpack.geoclaw import topotools
-import clawpack.geoclaw.topo as topo
+import clawpack.geoclaw.topotools as topotools
 import numpy as np
 
 
@@ -68,8 +68,9 @@ def maketopo1a(path):
     xupper = 2.5
     ylower = -1.
     yupper = 2.
-    topotools.topo2writer(path,topo1,xlower,xupper,ylower,yupper,\
-                          nxpoints,nypoints)
+    topotools.topo2writer(path, topo1, xlower, xupper, ylower, yupper,
+                          nxpoints, nypoints)
+
 
 def maketopo1b(path):
     """
@@ -81,8 +82,8 @@ def maketopo1b(path):
     xupper = 1.0
     ylower = 0.5
     yupper = 1.2
-    topotools.topo2writer(path,topo1,xlower,xupper,ylower,yupper,\
-                          nxpoints,nypoints)
+    topotools.topo2writer(path, topo1, xlower, xupper, ylower, yupper,
+                          nxpoints, nypoints)
 
 
 def test_topography_object(plot=False):
@@ -99,53 +100,56 @@ def test_topography_object(plot=False):
 
         hill_topo = []
         bowl_topo = []
-        topo_types = [2,3,1,2]
+        topo_types = [2, 3, 1, 2]
         for (n, topo_type) in enumerate(topo_types):
             bowl_path = os.path.join(base_path, 'bowl.tt%s' % topo_type)
             hill_path = os.path.join(base_path, 'hill.tt%s' % topo_type)
 
-            bowl_topo.append(topo.Topography(bowl_path))
-            hill_topo.append(topo.Topography(hill_path))
+            bowl_topo.append(topotools.Topography(bowl_path))
+            hill_topo.append(topotools.Topography(hill_path))
 
             if plot:
                 fig = plt.figure()
-                axes = fig.add_subplot(1,1,1)
-                hill_topo[-1].plot(axes=axes, limits=[-2000,0])
-                bowl_topo[-1].plot(axes=axes, limits=[-2000,0])
-                fig.suptitle('Bathymetry / topography, topo type = %s' % topo_type)
+                axes = fig.add_subplot(1, 1, 1)
+                hill_topo[-1].plot(axes=axes, limits=[-2000, 0])
+                bowl_topo[-1].plot(axes=axes, limits=[-2000, 0])
+                fig.suptitle(
+                         'Bathymetry / topography, topo type = %s' % topo_type)
                 plt.savefig('topotest%s.png' % (n + 2))
 
             print(n, topo_type)
             if n + 1 != len(topo_types):
-                bowl_path = os.path.join(base_path, 'bowl.tt%s' % topo_types[n+1])
-                hill_path = os.path.join(base_path, 'hill.tt%s' % topo_types[n+1])
+                bowl_path = os.path.join(
+                                    base_path, 'bowl.tt%s' % topo_types[n + 1])
+                hill_path = os.path.join(base_path,
+                                         'hill.tt%s' % topo_types[n + 1])
                 bowl_topo[-1].write(bowl_path)
                 hill_topo[-1].write(hill_path)
 
         # Check data
-        for (n,topo_type) in enumerate(topo_types):
-            for (m,topo_type) in enumerate(topo_types):
+        for (n, topo_type) in enumerate(topo_types):
+            for (m, topo_type) in enumerate(topo_types):
                 assert np.all(bowl_topo[n].X == bowl_topo[m].X), \
-                       "bowl[%s].X != bowl[%s].X" % (n,m)
+                       "bowl[%s].X != bowl[%s].X" % (n, m)
                 assert np.all(bowl_topo[n].Y == bowl_topo[m].Y), \
-                       "bowl[%s].Y != bowl[%s].Y" % (n,m)
+                       "bowl[%s].Y != bowl[%s].Y" % (n, m)
                 assert np.all(bowl_topo[n].Z == bowl_topo[m].Z), \
-                       "bowl[%s].Z != bowl[%s].Z" % (n,m)
+                       "bowl[%s].Z != bowl[%s].Z" % (n, m)
                 assert np.all(hill_topo[n].X == hill_topo[m].X), \
-                       "hill[%s].X != hill[%s].X" % (n,m)
+                       "hill[%s].X != hill[%s].X" % (n, m)
                 assert np.all(hill_topo[n].Y == hill_topo[m].Y), \
-                       "hill[%s].Y != hill[%s].Y" % (n,m)
+                       "hill[%s].Y != hill[%s].Y" % (n, m)
                 assert np.all(hill_topo[n].Z == hill_topo[m].Z), \
-                       "hill[%s].Z != hill[%s].Z" % (n,m)
+                       "hill[%s].Z != hill[%s].Z" % (n, m)
 
     finally:
-        paths = glob.glob(os.path.join(base_path,"*"))
+        paths = glob.glob(os.path.join(base_path, "*"))
         for path in paths:
             os.remove(path)
         os.rmdir(base_path)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print("Starting procedural test...")
     test1()
     print("Done performing procedural test.")
