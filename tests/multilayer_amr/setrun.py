ef40a103bcfeda430fe56d0b50dc2524b99963a3
Kyle Mandli <kyle.mandli@gmail.com>
2018-01-21 20:13:26 -0500

    Adjust for new variable definitioins

--- tests/multilayer_amr/setrun.py
+++ tests/multilayer_amr/setrun.py
@@ -141,7 +141,7 @@ def setrun(claw_pkg='geoclaw'):
     clawdata.num_eqn = 6
 
     # Number of auxiliary variables in the aux array (initialized in setaux)
-    clawdata.num_aux = 4 + rundata.multilayer_data.num_layers
+    clawdata.num_aux = 1 + rundata.multilayer_data.num_layers
 
     # Index of aux array corresponding to capacity function, if there is one:
     clawdata.capa_index = 0
@@ -478,6 +478,7 @@ def set_multilayer(rundata):
 
     # Physics parameters
     data.num_layers = 2
+    data.layer_index = 1
     data.eta = [0.0,-0.6]
     
     # Algorithm parameters
