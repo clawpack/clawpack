f2d05644f5982b6cfefa5e9ca9eaeac7a75807b8
Kyle Mandli <kyle.mandli@gmail.com>
2018-04-08 20:48:45 -0400

    Combine non-AMR and AMR multilayer test
    Also contains a new set of regression data.  The previous regression data
    had a buggy version of the friction coefficient setting.  This will hopefully
    reconcile all the changes to provide a better testing basis for the new AMR
    code.

--- tests/multilayer/setrun.py
+++ tests/multilayer/setrun.py
@@ -334,12 +334,12 @@ def setrun(claw_pkg='geoclaw'):
     amrdata = rundata.amrdata
 
     # max number of refinement levels:
-    amrdata.amr_levels_max = 1
+    amrdata.amr_levels_max = 3
 
     # List of refinement ratios at each level (length at least mxnest-1)
-    amrdata.refinement_ratios_x = [2,6]
-    amrdata.refinement_ratios_y = [2,6]
-    amrdata.refinement_ratios_t = [2,6]
+    amrdata.refinement_ratios_x = [2, 6]
+    amrdata.refinement_ratios_y = [2, 6]
+    amrdata.refinement_ratios_t = [2, 6]
 
 
     # Specify type of each aux variable in amrdata.auxtype.
