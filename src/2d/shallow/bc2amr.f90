7a238b2fae5b31c5c12edf18c5c66eef4d945d41
Kyle Mandli <kyle.mandli@gmail.com>
2018-08-03 21:18:00 -0400

    Add doxygen documentation

--- src/2d/shallow/bc2amr.f90
+++ src/2d/shallow/bc2amr.f90
@@ -1,73 +1,90 @@
-!
-!    Specific to geoclaw:  extrapolates aux(i,j,1) at boundaries
-!    to constant.
-!
 ! :::::::::: bc2amr ::::::::::::::::::::::::::::::::::::::::::::::;
+!> \callgraph
+!! \callergraph
+!!  Take a grid patch with mesh widths **hx**,**hy**, of dimensions **nrow** by
+!!  **ncol**,  and set the values of any piece of
+!!  of the patch which extends outside the physical domain 
+!!  using the boundary conditions. 
+!!
+!!  
+!!   Specific to geoclaw:  extrapolates aux(i,j,1) at boundaries
+!!   to constant.
+!!
+!!  ### Standard boundary condition choices for amr2ez in clawpack
+!!
+!!  At each boundary  k = 1 (left),  2 (right),  3 (bottom), 4 (top):
+!!
+!!  mthbc(k) =  
+!!  * 0  for user-supplied BC's (must be inserted!)
+!!  * 1  for zero-order extrapolation
+!!  * 2  for periodic boundary conditions
+!!  * 3  for solid walls, assuming this can be implemented
+!!                   by reflecting the data about the boundary and then
+!!                   negating the 2'nd (for k=1,2) or 3'rd (for k=3,4)
+!!                   component of q.
+!!  * 5  for sphere bcs (left half maps to right half of same side, and vice versa), as if domain folded in half
+!!
+!!  The corners of the grid patch are at 
+!!     (xlo_patch,ylo_patch)  --  lower left corner
+!!     (xhi_patch,yhi_patch) --  upper right corner
+!!
+!!  The physical domain itself is a rectangle bounded by
+!!     (xlower,ylower)  -- lower left corner
+!!     (xupper,yupper)  -- upper right corner
+!!  
+!   This figure below does not work with doxygen
+!   the picture is the following: 
+!  ____________________________________________________
+! 
+!                _____________________ (xupper,yupper)
+!               |                     |  
+!           ____|____ (xhi_patch,yhi_patch)   
+!           |   |    |                |
+!           |   |    |                |
+!           |   |    |                |
+!           |___|____|                |
+!  (xlo_patch,ylo_patch) |            |
+!               |                     |
+!               |_____________________|   
+!    (xlower,ylower)
+!  ____________________________________________________
+!!
+!!
+!>  Any cells that lie outside the physical domain are ghost cells whose
+!!  values should be set in this routine.  This is tested for by comparing
+!!  xlo_patch with xlower to see if values need to be set at the left
+!   as in the figure above, 
 !
-!     Take a grid patch with mesh widths hx,hy, of dimensions nrow by
-!     ncol,  and set the values of any piece of
-!     of the patch which extends outside the physical domain
-!     using the boundary conditions.
-!
-!     ------------------------------------------------
-!     # Standard boundary condition choices for amr2ez in clawpack
-!
-!     # At each boundary  k = 1 (left),  2 (right),  3 (top), 4 (bottom):
-!     #   mthbc(k) =  0  for user-supplied BC's (must be inserted!)
-!     #            =  1  for zero-order extrapolation
-!     #            =  2  for periodic boundary coniditions
-!     #            =  3  for solid walls, assuming this can be implemented
-!     #                  by reflecting the data about the boundary and then
-!     #                  negating the 2'nd (for k=1,2) or 3'rd (for k=3,4)
-!     #                  component of q.
-!     #            =  4  sphere bcs (left half maps to right half of same
-!     #                  side, and vice versa), as if domain folded in half
-!     ------------------------------------------------
-!
-!     The corners of the grid patch are at
-!        (xlo_patch,ylo_patch)  --  lower left corner
-!        (xhi_patch,yhi_patch) --  upper right corner
-!
-!     The physical domain itself is a rectangle bounded by
-!        (xlower,ylower)  -- lower left corner
-!        (xupper,yupper)  -- upper right corner
-!
-!     the picture is the following:
-!
-!               _____________________ (xupper,yupper)
-!              |                     |
-!          ____|____ (xhi_patch,yhi_patch) 
-!          |   |    |                |
-!          |   |    |                |
-!          |   |    |                |
-!          |___|____|                |
-! (xlo_patch,ylo_patch) |            |
-!              |                     |
-!              |_____________________|
-!   (xlower,ylower)
-!
-!
-!     Any cells that lie outside the physical domain are ghost cells whose
-!     values should be set in this routine.  This is tested for by comparing
-!     xlo_patch with xlower to see if values need to be set at the left, as in
-!     the figure above, and similarly at the other boundaries.
-!
-!     Patches are guaranteed to have at least 1 row of cells filled
-!     with interior values so it is possible to  extrapolate.
-!     Fix trimbd if you want more than 1 row pre-set.
-!
-!     Make sure the order the boundaries are specified is correct
-!     so that diagonal corner cells are also properly taken care of.
-!
-!     Periodic boundaries are set before calling this routine, so if the
-!     domain is periodic in one direction only you
-!     can safely extrapolate in the other direction.
-!
-!     Don't overwrite ghost cells in periodic directions!!
-!     This particular routine bc2amr_noslopesets auxillary values so
-!     that no slope in topography occurs at the physical boundary.
-!
+!>  and similarly at the other boundaries.
+!!  Patches are guaranteed to have at least 1 row of cells filled
+!!  with interior values so it is possible to extrapolate. 
+!!  Fix [trimbd()](@ref trimbd) if you want more than 1 row pre-set.
+!!
+!!  Make sure the order the boundaries are specified is correct
+!!  so that diagonal corner cells are also properly taken care of.
+!!
+!!  Periodic boundaries are set before calling this routine, so if the
+!!  domain is periodic in one direction only you
+!!  can safely extrapolate in the other direction. 
+!!
+!!  Don't overwrite ghost cells in periodic directions!
+!!
+!! \param val data array for solution \f$q \f$ (cover the whole grid **msrc**)
+!! \param aux data array for auxiliary variables 
+!! \param nrow number of cells in *i* direction on this grid
+!! \param ncol number of cells in *j* direction on this grid
+!! \param meqn number of equations for the system
+!! \param naux number of auxiliary variables
+!! \param hx spacing (mesh size) in *i* direction
+!! \param hy spacing (mesh size) in *j* direction
+!! \param level AMR level of this grid
+!! \param time setting ghost cell values at time **time**
+!! \param xlo_patch left bound of the input grid
+!! \param xhi_patch right bound of the input grid 
+!! \param ylo_patch lower bound of the input grid 
+!! \param yhi_patch upper bound of the input grid 
 ! ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+
 subroutine bc2amr(val,aux,nrow,ncol,meqn,naux, hx, hy, level, time,   &
                   xlo_patch, xhi_patch, ylo_patch, yhi_patch) 
 
