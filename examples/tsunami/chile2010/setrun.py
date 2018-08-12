8487c2f214968d54266cde6e9c0bd59c00d2b97b
Randy LeVeque <rjl@uw.edu>
2018-08-12 12:18:07 -0700

    fix comment in setrun.py since netcdf output doesn't work

--- examples/tsunami/chile2010/setrun.py
+++ examples/tsunami/chile2010/setrun.py
@@ -143,7 +143,7 @@ def setrun(claw_pkg='geoclaw'):
         clawdata.output_t0 = True
         
 
-    clawdata.output_format = 'ascii'      # 'ascii' or 'netcdf' 
+    clawdata.output_format = 'ascii'      # 'ascii' or 'binary' 
 
     clawdata.output_q_components = 'all'   # need all
     clawdata.output_aux_components = 'none'  # eta=h+B is in q
