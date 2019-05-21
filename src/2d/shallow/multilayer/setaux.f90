b7a1d6e9bf6d84fcf465199bdc8aa5c233d8c38c
Kyle Mandli <kyle.mandli@gmail.com>
2019-05-21 18:52:36 -0400

    Update multilayer setaux

--- src/2d/shallow/multilayer/setaux.f90
+++ src/2d/shallow/multilayer/setaux.f90
@@ -1,4 +1,3 @@
-subroutine setaux(mbc,mx,my,xlow,ylow,dx,dy,maux,aux)
 !     ============================================
 !
 !     # set auxiliary arrays
@@ -12,7 +11,10 @@ subroutine setaux(mbc,mx,my,xlow,ylow,dx,dy,maux,aux)
 !     aux(4:num_layers + 3,i,j) = Initial layer depths for linearized problem
 !
 
-    use amr_module, only: mcapa, xupper, yupper, xlower, ylower
+subroutine setaux(mbc,mx,my,xlow,ylow,dx,dy,maux,aux)
+
+    use amr_module, only: mcapa, xupper, yupper, xlower, ylower, NEEDS_TO_BE_SET
+    use amr_module, only: xperdom, yperdom
 
     use geoclaw_module, only: coordinate_system, earth_radius, deg2rad
     use geoclaw_module, only: sea_level, ambient_pressure
@@ -26,6 +28,8 @@ subroutine setaux(mbc,mx,my,xlow,ylow,dx,dy,maux,aux)
     use multilayer_module, only: eta_init, num_layers, aux_layer_index
 
     use topo_module
+
+    use adjoint_module, only : adjoint_flagging,innerprod_index
     
     implicit none
     
@@ -37,6 +41,7 @@ subroutine setaux(mbc,mx,my,xlow,ylow,dx,dy,maux,aux)
     ! Locals
     integer :: ii,jj,i,j,m,iint,jint,ilo,jlo
     real(kind=8) :: x,y,xm,ym,xp,yp,topo_integral
+    real(kind=8) :: xper,yper,xperm,yperm,xperp,yperp
     character(len=*), parameter :: aux_format = "(2i4,4d15.3)"
 
     ! Lat-Long coordinate system in use, check input variables
@@ -79,104 +84,129 @@ subroutine setaux(mbc,mx,my,xlow,ylow,dx,dy,maux,aux)
         aux(pressure_index, :, :) = ambient_pressure
     endif
 
+    ! Innerproduct field if used
+    if (adjoint_flagging) then
+        do jj=1-mbc,my+mbc
+            do ii=1-mbc,mx+mbc
+                if (aux(1,ii,jj) .eq. NEEDS_TO_BE_SET) then
+                    aux(innerprod_index,ii,jj) = 0.d0
+                    endif
+                enddo
+            enddo
+    endif
+
     ! Initial layer depths for multilayer
     aux(aux_layer_index:num_layers - 1 + aux_layer_index,:,:) = 0.d0 
 
+    ! ==========================================================================
+    !  Set Bathymetry
     ! Set analytical bathymetry here if requested
     if (test_topography > 0) then
-        forall (i=1-mbc:mx+mbc,j=1-mbc:my+mbc)
-            aux(1,i,j) = test_topo(xlow + (i - 0.5d0) * dx, &
-                                   ylow + (j - 0.5d0) * dy)
-        end forall
-    endif
+        do jj = 1 - mbc, my + mbc
+            ym = ylower + (jlo+jj-1.d0) * dy
+            yp = ylower + (jlo+jj) * dy
+            y = 0.5d0*(ym+yp)
+            do ii = 1 - mbc, mx + mbc
+                xm = xlower + (ilo+ii-1.d0) * dx
+                xp = xlower + (ilo+ii) * dx
+                x = 0.5d0*(xm+xp)
+                aux(1,ii,jj) = test_topo(x, y)
+            end do
+        end do
 
-! test:  compute integer indices based off same corner of domain 
-!        to reduce round off discrepancies
-    ilo = floor((xlow - xlower + .05d0*dx)/dx)
-    jlo = floor((ylow - ylower + .05d0*dy)/dy)
+    ! Set bathymetry based on input files
+    else if (mtopofiles > 0) then
 
-    ! Set bathymetry
-    do jj=1-mbc,my+mbc
-
-        !ym = ylow + (j - 1.d0) * dy
-        !y = ylow + (j - 0.5d0) * dy
-        !yp = ylow + real(j,kind=8) * dy
-
-        ym = ylower + (jlo+jj-1.d0) * dy
-        yp = ylower + (jlo+jj) * dy
-        y = 0.5d0*(ym+yp)
-
-        do ii=1-mbc,mx+mbc
-            !xm = xlow + (i - 1.d0) * dx
-            !x = xlow + (i - 0.5d0) * dx
-            !xp = xlow + real(i,kind=8) * dx
-
-            xm = xlower + (ilo+ii-1.d0) * dx
-            xp = xlower + (ilo+ii) * dx
-            x = 0.5d0*(xm+xp)
-
-            ! Set lat-long cell info
-            if (coordinate_system == 2) then
-                aux(2,ii,jj) = deg2rad * earth_radius**2 * (sin(yp * deg2rad) - sin(ym * deg2rad)) / dy
-                aux(3,ii,jj) = ym * deg2rad
-            endif
-            
-            ! skip setting aux(1,i,j) in ghost cell if outside physical domain
-            ! since topo files may not cover ghost cell, and values
-            ! should be extrapolated, which is done in next set of loops.
-            if ((y>yupper) .or. (y<ylower) .or. &
-                (x>xupper) .or. (x<xlower)) cycle
+        do jj=1-mbc,my+mbc
+            ym = ylower + (jlo+jj-1.d0) * dy
+            yp = ylower + (jlo+jj) * dy
+            y = 0.5d0*(ym+yp)
 
 
-            ! Use input topography files if available
-            if (mtopofiles > 0 .and. test_topography == 0) then
+            do ii=1-mbc,mx+mbc
+                xm = xlower + (ilo+ii-1.d0) * dx
+                xp = xlower + (ilo+ii) * dx
+                x = 0.5d0*(xm+xp)
+
+                ! Parameter NEEDS_TO_BE_SET initialized in amr_module.f90
+                ! saves time by otherwise copying instead of reinitializing
+                if (aux(1,ii,jj) .ne. NEEDS_TO_BE_SET) then
+                    cycle
+                endif
+
                 topo_integral = 0.d0
-                call cellgridintegrate(topo_integral,xm,x,xp,ym,y,yp, &
-                    xlowtopo,ylowtopo,xhitopo,yhitopo,dxtopo,dytopo, &
-                    mxtopo,mytopo,mtopo,i0topo,mtopoorder, &
-                    mtopofiles,mtoposize,topowork)
+                if ((y>yupper).or.(y<ylower).or.(x>xupper).or.(x<xlower)) then
+                    if (.not.(xperdom .or. yperdom)) then
+                        ! Skip setting as this cell sticks out of the physical
+                        ! domain and we are not setting periodic BCs
+                        cycle
 
+                    else
+                        ! We evaluate the periodic BC topography by computing
+                        ! the appropriate periodic grid cell coordinates and
+                        ! again calling cellgridintegrate
+                        call wrap_coords(x,y,xperm,xper,xperp,yperm,yper, &
+                                         yperp,dx,dy)
+                        call cellgridintegrate(topo_integral,  &
+                            xperm,xper,xperp,yperm,yper,yperp, &
+                            xlowtopo,ylowtopo,xhitopo,yhitopo,dxtopo,dytopo, &
+                            mxtopo,mytopo,mtopo,i0topo,mtopoorder, &
+                            mtopofiles,mtoposize,topowork)
+                    endif
+                else
+                    ! Cell does not extend outside of physical domain
+                    call cellgridintegrate(topo_integral,xm,x,xp,ym,y,yp, &
+                            xlowtopo,ylowtopo,xhitopo,yhitopo,dxtopo,dytopo, &
+                            mxtopo,mytopo,mtopo,i0topo,mtopoorder, &
+                            mtopofiles,mtoposize,topowork)
+                endif
 
+                ! Correct for geometry
                 if (coordinate_system == 2) then
                     aux(1,ii,jj) = topo_integral / (dx * dy * aux(2,ii,jj))
                 else
                     aux(1,ii,jj) = topo_integral / (dx * dy)
                 endif
+            enddo
+        enddo
+    else
+        print *, "ERROR:  There is no way to set bathymetry!  Either "
+        print *, "        provide topography files or request topography "
+        print*,  "        defined by a function."
+        stop
+    end if
 
+    ! Copy topo to ghost cells if outside physical domain and not periodic
+    if (.not. yperdom) then
+        do jj=1-mbc,my+mbc
+            y = ylower + (jlo+jj-.5d0) * dy
+            if ((y < ylower) .or. (y>yupper)) then
+                do ii=1-mbc,mx+mbc
+                    x = xlower + (ilo+ii-.5d0) * dx
+                    iint = ii + max(0, ceiling((xlower-x)/dx)) &
+                         - max(0, ceiling((x-xupper)/dx))
+                    jint = jj + max(0, ceiling((ylower-y)/dy)) &
+                         - max(0, ceiling((y-yupper)/dy))
+                    aux(1,ii,jj) = aux(1,iint,jint)
+                enddo
             endif
         enddo
-    enddo
-
-    ! Copy topo to ghost cells if outside physical domain
-
-   do jj=1-mbc,my+mbc
-        y = ylower + (jlo+jj-.5d0) * dy
-        if ((y < ylower) .or. (y>yupper)) then
-            do ii=1-mbc,mx+mbc
-                x = xlower + (ilo+ii-.5d0) * dx
-                iint = ii + max(0, ceiling((xlower-x)/dx)) &
-                         -  max(0, ceiling((x-xupper)/dx))
-                jint = jj + max(0, ceiling((ylower-y)/dy)) &
-                         -  max(0, ceiling((y-yupper)/dy))
-                aux(1,ii,jj) = aux(1,iint,jint)
-            enddo
-        endif
-    enddo
-
-    do ii=1-mbc,mx+mbc
-        x =  xlower + (ilo+ii-.5d0) * dx
-        if ((x < xlower) .or. (x > xupper)) then
-            do jj=1-mbc,my+mbc
-                y = ylower + (jlo+jj-.5d0) * dy
-                iint = ii + max(0, ceiling((xlower-x)/dx)) &
-                          - max(0, ceiling((x-xupper)/dx))
-                jint = jj + max(0, ceiling((ylower-y)/dy)) &
-                          - max(0, ceiling((y-yupper)/dy))
-                aux(1,ii,jj) = aux(1,iint,jint)
-            enddo
-        endif
-    enddo  
-
+    endif
+    if (.not. xperdom) then
+        do ii=1-mbc,mx+mbc
+            x =  xlower + (ilo+ii-.5d0) * dx
+            if ((x < xlower) .or. (x > xupper)) then
+                do jj=1-mbc,my+mbc
+                    y = ylower + (jlo+jj-.5d0) * dy
+                    iint = ii + max(0, ceiling((xlower-x)/dx)) &
+                         - max(0, ceiling((x-xupper)/dx))
+                    jint = jj + max(0, ceiling((ylower-y)/dy)) &
+                         - max(0, ceiling((y-yupper)/dy))
+                    aux(1,ii,jj) = aux(1,iint,jint)
+                enddo
+            endif
+        enddo
+    endif
 
     ! Set friction coefficient based on a set of depth levels
     if (friction_index > 0) then
@@ -214,17 +244,38 @@ subroutine setaux(mbc,mx,my,xlow,ylow,dx,dy,maux,aux)
         enddo
     enddo
 
-    ! Output for debugging
-    if (.false.) then
-        open(23, file='fort.aux',status='unknown',form='formatted')
-        print *,'Writing out aux arrays'
-        print *,' '
-        do j=1,my
-            do i=1,mx
-                write(23,*) i,j,(aux(m,i,j),m=1,maux)
-            enddo
-        enddo
-        close(23)
-    endif
+contains
+
+    ! Provide wrapper function for providing periodic coordinates
+    subroutine wrap_coords(x,y,xperm,xper,xperp,yperm,yper,yperp,dx,dy)
+
+        use amr_module, only: xperdom, yperdom, xupper, yupper, xlower, ylower
+
+        implicit none
+
+        ! Arguments
+        real(kind=8), intent(in) :: x,y,dx,dy
+        real(kind=8), intent(out) :: xperm,xper,xperp,yperm,yper,yperp
+        real(kind=8) :: xdomain, ydomain
+
+        ! need size of domain for wrapping
+        xdomain = xupper - xlower
+        ydomain = yupper - ylower
+        xper = x
+        yper = y
+
+        ! test for wrapping of coordinates
+        if (x > xupper .and. xperdom) xper = xper - xdomain
+        if (x < xlower .and. xperdom) xper = xper + xdomain
+        if (y > yupper .and. yperdom) yper = yper - ydomain
+        if (y < ylower .and. yperdom) yper = yper + ydomain
+
+        ! adjust rest of variables
+        xperm = xper - 0.5d0 * dx
+        xperp = xper + 0.5d0 * dx
+        yperm = yper - 0.5d0 * dy
+        yperp = yper + 0.5d0 * dy
+
+    end subroutine wrap_coords
     
 end subroutine setaux
