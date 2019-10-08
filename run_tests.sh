df59af6a59430518737c678089d6c6d94590f10d
Ian Bolliger <bolliger32@gmail.com>
2019-10-07 22:37:09 -0700

    try again

--- run_tests.sh
+++ run_tests.sh
@@ -18,18 +18,18 @@ echo "------------------------------"
 
 echo "testing pyclaw"
 cd $CLAW/pyclaw/examples
-nosetests | for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
+nosetests || for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
 
 echo "testing classic"
 cd $CLAW/classic
-nosetests | for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
+nosetests || for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
 
 
 echo "testing amrclaw"
 cd $CLAW/amrclaw
-nosetests | for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
+nosetests || for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
 
 
 echo "testing geoclaw"
 cd $CLAW/geoclaw
-nosetests | for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
+nosetests || for failed_test_path in *_output ; do cat $failed_test_path/run_output.txt ; cat $failed_test_path/error_output.txt ; done
