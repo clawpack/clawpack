de9bd13ab5ae7f344facd06e5916d67a19c3656c
David Ketcheson <dketch@gmail.com>
2023-11-02 12:00:14 +0300

    Properly manage import directories for editable and non-editable installs.

--- clawpack/__init__.py
+++ clawpack/__init__.py
@@ -14,15 +14,13 @@ _subpackages = {
 
 __all__ = list(_subpackages.keys())
 
-CLAW_dir = _os.environ.get('CLAW')
-if CLAW_dir == None:
-    raise Exception('You must set the CLAW environment to use an editable install.')
-
-_root = _os.path.dirname(_os.path.join(CLAW_dir, 'clawpack'))
-_path = (_os.path.join(_root, *_sdir.split('/'))
-         for _sdir in set(_subpackages.values()))
-__path__.extend(_sdir for _sdir in _path
-                if _os.path.isdir(_sdir))
-del _root, _path
+_init = _os.path.abspath(__file__)
+_root = _os.path.dirname(_os.path.dirname(_init))
+_path = (
+    _os.path.join(_root, *_sdir.split('/'))
+    for _sdir in set(_subpackages.values())
+)
+__path__.extend(filter(_os.path.isdir, _path))
+del _os, _init, _root, _path
 
 __version__ = '5.9.1'   # must also be changed in setup.py
